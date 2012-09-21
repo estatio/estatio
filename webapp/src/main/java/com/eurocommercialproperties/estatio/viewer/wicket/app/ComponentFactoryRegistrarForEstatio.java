e809b01909beb082d55067fecf46cc88de92e4d0
Dan Haywood <dan@haywood-associates.co.uk>
2012-09-21 10:37:11 +0100

    moved the autocomplete functionality back into isis core, ...
    ... so estatio-specific customizations no longer required.

--- webapp/src/main/java/com/eurocommercialproperties/estatio/viewer/wicket/app/ComponentFactoryRegistrarForEstatio.java
+++ webapp/src/main/java/com/eurocommercialproperties/estatio/viewer/wicket/app/ComponentFactoryRegistrarForEstatio.java
@@ -2,9 +2,6 @@ package com.eurocommercialproperties.estatio.viewer.wicket.app;
 
 import com.google.inject.Singleton;
 
-import org.apache.isis.viewer.wicket.ui.components.widgets.entitylinkselect2.EntityLinkSelect2Factory;
-import org.apache.isis.viewer.wicket.ui.components.widgets.valuechoices.ValueChoicesComponentFactory;
-import org.apache.isis.viewer.wicket.ui.components.widgets.valuechoicesselect2.ValueChoicesSelect2ComponentFactory;
 import org.apache.isis.viewer.wicket.viewer.registries.components.ComponentFactoryRegistrarDefault;
 
 @Singleton
@@ -13,7 +10,6 @@ public class ComponentFactoryRegistrarForEstatio extends ComponentFactoryRegistr
     @Override
     public void addComponentFactories(ComponentFactoryList componentFactories) {
         super.addComponentFactories(componentFactories);
-        componentFactories.replace(new EntityLinkSelect2Factory());
-        componentFactories.replace(ValueChoicesComponentFactory.class, new ValueChoicesSelect2ComponentFactory());
+        // currently no replacements
     }
 }
