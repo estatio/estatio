cdbca2c67e9007db20b0dcd36bfd9e6de0b158c6
Jeroen van der Wal <jeroen@stromboli.it>
2013-01-30 10:57:21 +0100

    added indexfixture to administration menu

--- webapp/src/main/java/org/estatio/viewer/wicket/app/administration/EstatioAdministrationService.java
+++ webapp/src/main/java/org/estatio/viewer/wicket/app/administration/EstatioAdministrationService.java
@@ -1,9 +1,12 @@
 package org.estatio.viewer.wicket.app.administration;
 
+import org.estatio.dom.index.Indices;
+import org.estatio.fixture.EstatioFixture;
+import org.estatio.fixture.index.IndexFixture;
+import org.estatio.viewer.wicket.app.scheduler.EstatioSchedulerService;
+
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.Named;
-import org.estatio.fixture.EstatioFixture;
-import org.estatio.viewer.wicket.app.scheduler.EstatioSchedulerService;
 
 
 @Named("Administration")
@@ -19,17 +22,40 @@ public class EstatioAdministrationService {
         scheduler.initializeJobs();
     }
     
-    // {{ injected: Container
+    public String installIndexFixture() {
+        IndexFixture fixture = container.newTransientInstance(IndexFixture.class);
+        fixture.install();
+        return "Index fixture successfully installed";
+    }
+    
+    public String disableInstallIndexFixture() {
+        if (indexRepo.allIndices().size() == 0) {
+            return null;
+        }
+        return "Index fixture already installed";
+    }
+
+    // {{ injected
     private DomainObjectContainer container;
     
     public void setContainer(DomainObjectContainer container) {
         this.container = container;
     }
-    // }}
     
     private EstatioSchedulerService scheduler;
     
     public void setSchedulerService(EstatioSchedulerService scheduler){
         this.scheduler = scheduler;
     }
+    
+    private Indices indexRepo;
+    
+    public void SetIndexRepo(Indices indices) {
+        this.indexRepo = indices;
+    }
+    // }}
+    
+    
+    
+    
 }
