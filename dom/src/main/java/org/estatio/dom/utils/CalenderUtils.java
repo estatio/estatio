adae2b8a6ff4f3a6cd3b3438c64c518c591af017
Jeroen van der Wal <jeroen@stromboli.it>
2013-02-01 18:28:36 +0100

    wip

--- dom/src/main/java/org/estatio/dom/utils/CalenderUtils.java
+++ dom/src/main/java/org/estatio/dom/utils/CalenderUtils.java
@@ -16,8 +16,8 @@ public class CalenderUtils {
     public static Interval currentInterval(LocalDate date, String rrule) {
         return currentInterval(date, rrule, new LocalDate(2000, 1, 1));
     }
-    
-    public static Interval currentInterval(LocalDate date, String rrule, LocalDate startDate){
+
+    public static Interval currentInterval(LocalDate date, String rrule, LocalDate startDate) {
         LocalDate nextDate;
 
         LocalDateIterator iter;
@@ -37,8 +37,58 @@ public class CalenderUtils {
         return null;
     }
 
-    public static LocalDate nextDate(LocalDate date, String rrule){
+    public static LocalDate nextDate(LocalDate date, String rrule) {
         return currentInterval(date, rrule).getEnd().toLocalDate();
     }
-    
+
+    public static boolean isBetween(LocalDate date, LocalDate startDate, LocalDate endDate) {
+        if (date.compareTo(startDate) >= 0 && (endDate == null || date.compareTo(endDate) <= 0)) {
+            return true;
+        }
+        ;
+        return false;
+    }
+
+    public class MyInterval {
+
+        private LocalDate startDate;
+        private LocalDate endDate;
+        private MyInterval parentInterval;
+
+        public MyInterval(LocalDate startDate, LocalDate endDate) {
+            super();
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public LocalDate getStartDate() {
+            return startDate == null ? new LocalDate(1900, 1, 1) : startDate;
+        }
+
+        public void setStartDate(LocalDate startDate) {
+            this.startDate = startDate;
+        }
+
+        public LocalDate getEndDate() {
+            return endDate == null ? new LocalDate(2999, 1, 1) : endDate;
+        }
+
+        public void setEndDate(LocalDate endDate) {
+            this.endDate = endDate;
+        }
+
+        public void setParentInterval(MyInterval parentInterval) {
+            this.parentInterval = parentInterval;
+        }
+
+        public boolean isFullInterval() {
+            return (getStartDate().compareTo(parentInterval.getStartDate()) <= 0 && getEndDate().compareTo(parentInterval.getEndDate()) >= 0);
+        }
+
+        public boolean isWithinParent() {
+            return !(getStartDate().compareTo(parentInterval.getEndDate()) >= 0 || this.getEndDate().compareTo(parentInterval.getStartDate()) <= 0);
+
+        }
+
+    }
 }
