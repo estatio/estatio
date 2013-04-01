5b1c88ea496aae071c487754cb34452de527ad8d
Jeroen van der Wal <jeroen@stromboli.it>
2013-04-01 17:36:07 +0200

    hide creation of communication channels from repository

--- dom/src/main/java/org/estatio/dom/communicationchannel/CommunicationChannels.java
+++ dom/src/main/java/org/estatio/dom/communicationchannel/CommunicationChannels.java
@@ -2,11 +2,8 @@ package org.estatio.dom.communicationchannel;
 
 import java.util.List;
 
-
 import org.apache.isis.applib.AbstractFactoryAndRepository;
-import org.apache.isis.applib.annotation.ActionSemantics;
-import org.apache.isis.applib.annotation.ActionSemantics.Of;
-import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.Named;
 import org.apache.isis.applib.annotation.Prototype;
 import org.estatio.dom.geography.Country;
@@ -26,17 +23,10 @@ public class CommunicationChannels extends AbstractFactoryAndRepository {
     }
 
     // }}
-    
+
     // {{ newPostalAddress
-    @ActionSemantics(Of.SAFE)
-    @MemberOrder(sequence = "1")
-    public PostalAddress newPostalAddress(
-            final @Named("Address 1") String address1, 
-            final @Named("Address 2") String address2, 
-            final @Named("Postal Code") String postalCode, 
-            final @Named("City") String city,
-            final State state, 
-            final Country country) {
+    @Hidden
+    public PostalAddress newPostalAddress(final @Named("Address 1") String address1, final @Named("Address 2") String address2, final @Named("Postal Code") String postalCode, final @Named("City") String city, final State state, final Country country) {
         final PostalAddress pa = newTransientInstance(PostalAddress.class);
         pa.setType(CommunicationChannelType.POSTAL_ADDRESS);
         pa.setAddress1(address1);
@@ -48,57 +38,50 @@ public class CommunicationChannels extends AbstractFactoryAndRepository {
         persist(pa);
         return pa;
     }
+
     // }}
 
     // {{ newEmailAddress
-    @MemberOrder(sequence = "2")
-    public EmailAddress newEmailAddress(
-            final @Named("Address") String address) {
+    @Hidden
+    public EmailAddress newEmailAddress(final @Named("Address") String address) {
         final EmailAddress ea = newTransientInstance(EmailAddress.class);
         ea.setType(CommunicationChannelType.EMAIL_ADDRESS);
         ea.setAddress(address);
         persist(ea);
         return ea;
     }
+
     // }}
 
     // {{ newPhoneNumber
-    @MemberOrder(sequence = "3")
-    public PhoneNumber newPhoneNumber(
-            final @Named("Number") String number) {
+    @Hidden
+    public PhoneNumber newPhoneNumber(final @Named("Number") String number) {
         final PhoneNumber pn = newTransientInstance(PhoneNumber.class);
         pn.setType(CommunicationChannelType.PHONE_NUMBER);
         pn.setPhoneNumber(number);
         persist(pn);
         return pn;
     }
+
     // }}
- 
+
     // {{ newFaxNumber
-    @MemberOrder(sequence = "4")
-    public FaxNumber newFaxNumber(
-            final @Named("Number") String number) {
+    @Hidden
+    public FaxNumber newFaxNumber(final @Named("Number") String number) {
         final FaxNumber fn = newTransientInstance(FaxNumber.class);
         fn.setType(CommunicationChannelType.FAX_NUMBER);
         fn.setFaxNumber(number);
         persist(fn);
         return fn;
     }
+
     // }}
 
     // {{ AllCommunicationChannel
     @Prototype
-    public List<CommunicationChannel> allCommunicationChannel() {
+    public List<CommunicationChannel> allCommunicationChannels() {
         return allInstances(CommunicationChannel.class);
     }
     // }}
-
-    // {{ allPostalAddresses
-    @Prototype
-    public List<PostalAddress> allPostalAddresses() {
-        return allInstances(PostalAddress.class);
-    }
-    // }}
-
 
 }
