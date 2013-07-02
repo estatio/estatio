483a4b1522838eaf0a4b4f04edb27c3a1ec52622
Dan Haywood <dan@haywood-associates.co.uk>
2013-07-02 15:03:00 +0100

    moving mothballed -> app/mothballed

---
+++ mothballed/dnplugins/src/test/java/org/estatio/dnplugins/EstatioIdentifierFactoryTest_stripOidSuffixIfPresent.java
@@ -0,0 +1,25 @@
+package org.estatio.dnplugins;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.estatio.dnplugins.EstatioIdentifierFactory;
+import org.junit.Test;
+
+public class EstatioIdentifierFactoryTest_stripOidSuffixIfPresent {
+
+
+    @Test
+    public void whenIs() {
+        final String actual = EstatioIdentifierFactory.stripOidSuffixIfPresent("ABC_ID_OID");
+        assertThat(actual, is("ABC_ID"));
+    }
+
+
+    @Test
+    public void whenIsNot() {
+        final String actual = EstatioIdentifierFactory.stripOidSuffixIfPresent("ABC_ID_FOO");
+        assertThat(actual, is("ABC_ID_FOO"));
+    }
+
+}
