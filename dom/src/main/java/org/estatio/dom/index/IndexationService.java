dbff6734c3b10c391c82c033338d117a0e80b6e2
Dan Haywood <dan@haywood-associates.co.uk>
2013-11-15 10:41:47 +0000

    refactoring IndexationService#indexate (checkstyle flags method is too long)
    introduce concept of an IndexationResult, and use this to apply changes to the Indexable input.

--- dom/src/main/java/org/estatio/dom/index/IndexationService.java
+++ dom/src/main/java/org/estatio/dom/index/IndexationService.java
@@ -37,6 +37,8 @@ public class IndexationService {
         BigDecimal baseIndexValue = null;
         BigDecimal nextIndexValue = null;
 
+        final IndexationResult indexationResult;
+
         if (index != null) {
             index.initialize(input);
             baseIndexValue = input.getBaseIndexValue();
@@ -66,12 +68,39 @@ public class IndexationService {
 
                 }
             }
+            
+            indexationResult = new IndexationResult(indexedValue, indexationPercentage, baseIndexValue, nextIndexValue);
+        } else {
+            indexationResult = IndexationResult.NULL;
         }
 
-        input.setBaseIndexValue(baseIndexValue);
-        input.setNextIndexValue(nextIndexValue);
-        input.setIndexationPercentage(indexationPercentage);
-        input.setIndexedValue(indexedValue);
+        indexationResult.apply(input);
+    }
+
+    static class IndexationResult {
+        
+        public static final IndexationResult NULL = new IndexationResult(null, null, null, null);
+        
+        private BigDecimal indexedValue = null;
+        private BigDecimal indexationPercentage = null;
+        private BigDecimal baseIndexValue = null;
+        private BigDecimal nextIndexValue = null;
+ 
+        public IndexationResult(
+                final BigDecimal indexedValue, final BigDecimal indexationPercentage, 
+                final BigDecimal baseIndexValue, final BigDecimal nextIndexValue) {
+            this.indexedValue = indexedValue;
+            this.indexationPercentage = indexationPercentage;
+            this.baseIndexValue = baseIndexValue;
+            this.nextIndexValue = nextIndexValue;
+        }
+
+        public void apply(final Indexable indexable) {
+            indexable.setBaseIndexValue(baseIndexValue);
+            indexable.setIndexationPercentage(indexationPercentage);
+            indexable.setNextIndexValue(nextIndexValue);
+            indexable.setIndexedValue(indexedValue);
+        }
     }
 
 }
