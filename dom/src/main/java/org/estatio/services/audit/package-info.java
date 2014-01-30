941eedc499bcdafcf6ec391c98da5387179f70df
Dan Haywood <dan@haywood-associates.co.uk>
2014-01-30 09:01:44 +0000

    Removing AuditingServiceForEstatio (on back of changes in ISIS-660). Also, (once-again) added in example CountPropertiesJob.

--- dom/src/main/java/org/estatio/services/audit/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Estatio-specific {@link org.estatio.services.audit.AuditingServiceForEstatio customisation} of Isis' 
- * {@link org.apache.isis.applib.services.audit.AuditingService AuditingService}.  
- * 
- * <p>
- * The implementation simply persists the audit information as 
- * {@link org.estatio.services.audit.AuditEntryForEstatio audit entry}.
- */
-package org.estatio.services.audit;
