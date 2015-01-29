2ef6862c70e4e971cf4f52d3e4c96bc85d3d5ee0
Sander Ginn <sanderdeginno@gmail.com>
2015-01-29 17:28:21 +0100

    All LeaseTerms finders now tested + refactored deprecated annotations

--- estatioapp/integtests/src/test/java/org/estatio/integtests/lease/LeaseTermsTest.java
+++ estatioapp/integtests/src/test/java/org/estatio/integtests/lease/LeaseTermsTest.java
@@ -18,44 +18,69 @@
  */
 package org.estatio.integtests.lease;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.math.BigDecimal;
 import java.util.List;
+
 import javax.inject.Inject;
+
+import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
 import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+import org.estatio.dom.asset.Property;
+import org.estatio.dom.lease.Lease;
+import org.estatio.dom.lease.LeaseItemType;
+import org.estatio.dom.lease.LeaseItems;
 import org.estatio.dom.lease.LeaseTerm;
 import org.estatio.dom.lease.LeaseTermForIndexable;
 import org.estatio.dom.lease.LeaseTerms;
+import org.estatio.dom.lease.Leases;
 import org.estatio.fixture.EstatioBaseLineFixture;
+import org.estatio.fixture.lease.LeaseForOxfTopModel001;
 import org.estatio.fixture.lease.LeaseItemAndTermsForOxfTopModel001;
 import org.estatio.integtests.EstatioIntegrationTest;
 import org.estatio.integtests.VT;
 
-import static org.hamcrest.CoreMatchers.is;
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LeaseTermsTest extends EstatioIntegrationTest {
 
+    @Before
+    public void setupData() {
+        runScript(new FixtureScript() {
+            @Override
+            protected void execute(ExecutionContext executionContext) {
+                executionContext.executeChild(this, new EstatioBaseLineFixture());
+
+                executionContext.executeChild(this, new LeaseItemAndTermsForOxfTopModel001());
+            }
+        });
+    }
+
+    @Inject
+    Leases leases;
+
+    @Inject
+    LeaseItems leaseItems;
+
+    @Inject
+    LeaseTerms leaseTerms;
+
+    Lease lease;
+
+    @Before
+    public void setUp() throws Exception {
+        lease = leases.findLeaseByReference(LeaseForOxfTopModel001.LEASE_REFERENCE);
+    }
+
     public static class AllLeaseTerms extends LeaseTermsTest {
-
-        @Before
-        public void setupData() {
-            runScript(new FixtureScript() {
-                @Override
-                protected void execute(ExecutionContext executionContext) {
-                    executionContext.executeChild(this, new EstatioBaseLineFixture());
-
-                    executionContext.executeChild(this, new LeaseItemAndTermsForOxfTopModel001());
-                }
-            });
-        }
-
-        @Inject
-        private LeaseTerms leaseTerms;
 
         @Test
         public void whenExists() throws Exception {
@@ -76,5 +101,27 @@ public class LeaseTermsTest extends EstatioIntegrationTest {
             Assert.assertEquals(VT.bd("20000.00"), baseValue);
         }
 
+    }
+
+    public static class FindByPropertyAndTypeAndStartDate extends LeaseTermsTest {
+
+        @Test
+        public void findByPropertyAndTypeAndStartDate() throws Exception {
+            Property property = lease.getProperty();
+            List<LeaseTerm> results = leaseTerms.findByPropertyAndTypeAndStartDate(property, LeaseItemType.RENT, lease.getStartDate());
+            assertThat(results.size(), is(1));
+            assertThat(results.get(0), is(lease.getItems().first().getTerms().first()));
+        }
+    }
+
+    public static class FindStartDatesByPropertyAndType extends LeaseTermsTest {
+
+        @Test
+        public void findStartDatesByPropertyAndType() throws Exception {
+            Property property = lease.getProperty();
+            List<LocalDate> results = leaseTerms.findStartDatesByPropertyAndType(property, LeaseItemType.RENT);
+            assertThat(results.size(), is(1));
+            assertThat(results.get(0), is(lease.getItems().first().getTerms().first().getStartDate()));
+        }
     }
 }
