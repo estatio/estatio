4cac9cabca50e1c65292a3da3db10ae8800df890
Jeroen van der Wal <jeroen@stromboli.it>
2016-03-30 19:00:07 +0200

    EST-665: Prevent verify on terms that might have been deleted on the same verification process

--- estatioapp/dom/src/main/java/org/estatio/dom/lease/LeaseItem.java
+++ estatioapp/dom/src/main/java/org/estatio/dom/lease/LeaseItem.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.jdo.annotations.IdGeneratorStrategy;
@@ -605,11 +606,9 @@ public class LeaseItem
 
     @Action(semantics = SemanticsOf.IDEMPOTENT)
     public LeaseItem verifyUntil(final LocalDate date) {
-        for(LeaseTerm term : getTerms()) {
+        for(LeaseTerm term : getTerms().stream().filter(t->t.getPrevious()==null).collect(Collectors.toList())) {
             // only verify the first terms of a chain or the standalones:
-            if (term.getPrevious() == null) {
-                term.verifyUntil(date);
-            }
+            term.verifyUntil(date);
         }
         return this;
     }
