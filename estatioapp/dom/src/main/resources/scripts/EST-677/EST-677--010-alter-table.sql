7ab1ec0e2aa3969ab3e421fbd7d814a60cb71a91
Johan Doornenbal <johan@filternet.nl>
2016-04-18 07:49:41 +0200

    upgrade sql script now idempotent

--- estatioapp/dom/src/main/resources/scripts/EST-677/EST-677--010-alter-table.sql
+++ estatioapp/dom/src/main/resources/scripts/EST-677/EST-677--010-alter-table.sql
@@ -9,9 +9,16 @@ GO
 
 ---------------------------
 
+IF NOT EXISTS (SELECT *
+  FROM [INFORMATION_SCHEMA].[COLUMNS]
+  WHERE TABLE_NAME = 'LeaseType'
+  AND COLUMN_NAME = 'atPath')
+BEGIN
 ALTER TABLE [dbo].[LeaseType]  ADD atPath [varchar](255)
-UPDATE [dbo].[LeaseType] SET atPath = '/FRA' WHERE reference IN ('BC','BD','BX','CO','D','HB','OP','P')
-UPDATE [dbo].[LeaseType] SET atPath = '/FRA' WHERE reference IN ('FRBC','FRBD','FRBX','FRCO','FRD','FRHB','FROP','FRP')
+UPDATE [dbo].[LeaseType] SET atPath = '/FRA' WHERE reference IN ('BC','BD','BX','CO','D','HB','OP','P') AND atPath IS NULL
+UPDATE [dbo].[LeaseType] SET atPath = '/FRA' WHERE reference IN ('FRBC','FRBD','FRBX','FRCO','FRD','FRHB','FROP','FRP') AND atPath IS NULL
 UPDATE [dbo].[LeaseType] SET atPath = '/ITA' WHERE atPath IS NULL
 ALTER TABLE [dbo].[LeaseType] ALTER COLUMN  atPath [varchar](255) NOT NULL
+END
+
 GO
