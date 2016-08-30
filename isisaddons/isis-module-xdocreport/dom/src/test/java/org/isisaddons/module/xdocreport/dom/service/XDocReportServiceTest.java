f570107ad75b598bb67b47bde572c30c83f23946
Dan Haywood <dan@haywood-associates.co.uk>
2016-08-30 09:09:25 +0100

    ISIS-800: adds an empty module for xdocreport add-on

---
+++ isisaddons/isis-module-xdocreport/dom/src/test/java/org/isisaddons/module/xdocreport/dom/service/XDocReportServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2013~2014 Dan Haywood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.isisaddons.module.xdocreport.dom.service;
+
+import org.jmock.auto.Mock;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import org.apache.isis.applib.services.config.ConfigurationService;
+import org.apache.isis.core.unittestsupport.jmocking.JUnitRuleMockery2;
+
+public class XDocReportServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Rule
+    public JUnitRuleMockery2 context = JUnitRuleMockery2.createFor(JUnitRuleMockery2.Mode.INTERFACES_AND_CLASSES);
+
+    @Mock
+    ConfigurationService mockConfigurationService;
+
+    XDocReportService service;
+
+    @Before
+    public void setUp() throws Exception {
+        service = new XDocReportService();
+    }
+
+    @Test
+    public void simple() throws Exception {
+
+        // given
+
+        // when
+
+        // then
+    }
+
+}
