5ea44061761eb29fa4e1ba3d571607b3b7659a7f
Dan Haywood <dan@haywood-associates.co.uk>
2016-10-18 15:08:21 +0100

    EST-870: fixes broken mixin for phoneNumberTitles on Organisation.  Also:
    - minor tweaks to layout of Organisation itself (general/application level/metadata, also eagerly load relationship types)
    - similarly Person layout

--- incode/communications/dom/src/main/java/org/incode/module/communications/dom/impl/commchannel/CommunicationChannelOwner_phoneNumberTitles.java
+++ incode/communications/dom/src/main/java/org/incode/module/communications/dom/impl/commchannel/CommunicationChannelOwner_phoneNumberTitles.java
@@ -1,36 +1,9 @@
 package org.incode.module.communications.dom.impl.commchannel;
 
-import javax.inject.Inject;
-
-import org.apache.isis.applib.annotation.Action;
-import org.apache.isis.applib.annotation.ActionLayout;
-import org.apache.isis.applib.annotation.Contributed;
-import org.apache.isis.applib.annotation.Property;
-import org.apache.isis.applib.annotation.PropertyLayout;
-import org.apache.isis.applib.annotation.SemanticsOf;
-import org.apache.isis.applib.annotation.Where;
-
-public abstract class CommunicationChannelOwner_phoneNumberTitles {
-
-
-    private final CommunicationChannelOwner owner;
-    private String separator;
+public abstract class CommunicationChannelOwner_phoneNumberTitles extends CommunicationChannelOwner_communicationChannelTitlesAbstract {
 
     public CommunicationChannelOwner_phoneNumberTitles(final CommunicationChannelOwner owner, final String separator) {
-        this.owner = owner;
-        this.separator = separator;
+        super(owner, separator, CommunicationChannelType.PHONE_NUMBER);
     }
-
-    @Action(semantics = SemanticsOf.SAFE)
-    @ActionLayout(contributed = Contributed.AS_ASSOCIATION)
-    @Property(hidden = Where.OBJECT_FORMS)
-    @PropertyLayout(typicalLength = CommunicationChannel.PhoneNumberType.MAX_LEN)
-    public String $$() {
-        return communicationChannelTitleService.channelTitleJoined(owner, CommunicationChannelType.EMAIL_ADDRESS,
-                separator);
-    }
-
-    @Inject
-    CommunicationChannelTitleService communicationChannelTitleService;
 
 }
