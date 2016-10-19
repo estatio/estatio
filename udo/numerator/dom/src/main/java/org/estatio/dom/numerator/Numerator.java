2e5306907e9ad4b4c7fb78838f8d64edacda3992
Dan Haywood <dan@haywood-associates.co.uk>
2016-10-19 15:29:01 +0100

    EST-874: reverts .orm overrides for udo-numerator, and hard-codes the schema to 'dbo'.

--- udo/numerator/dom/src/main/java/org/estatio/dom/numerator/Numerator.java
+++ udo/numerator/dom/src/main/java/org/estatio/dom/numerator/Numerator.java
@@ -66,7 +66,7 @@ import lombok.Setter;
  */
 @javax.jdo.annotations.PersistenceCapable(
         identityType = IdentityType.DATASTORE
-        ,schema = "IncodeNumerator" // Isis' ObjectSpecId inferred from @DomainObject#objectType
+        ,schema = "dbo" // Isis' ObjectSpecId inferred from @DomainObject#objectType
 )
 @javax.jdo.annotations.DatastoreIdentity(
         strategy = IdGeneratorStrategy.IDENTITY,
