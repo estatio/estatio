5f91adba78a3e5bd47fbf56627ed9323485e8e25
Jeroen van der Wal <jeroen@stromboli.it>
2016-11-22 17:43:03 +0100

    EST-944: Add addition sql conversion scripts

---
+++ estatioapp/app/src/main/resources/dbscripts/1.8.0/EST-921/EST-921-00-drop-tables.sql
@@ -0,0 +1,4 @@
+DROP TABLE PartitionItem
+GO
+DROP TABLE Partitioning
+GO
--- estatioapp/app/src/main/resources/dbscripts/upgrade.sh
+++ estatioapp/app/src/main/resources/dbscripts/upgrade.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 if [ ! $# -eq 5 ]
       then
-              echo "Usage: upgrade <server> <username> <password> <database> <sql.script.list>"
+              echo "Usage: upgrade <server> <username> <password> <database> <version>"
               exit 0
 fi
 server=$1
 username=$2
 password=$3
 database=$4
-filelist=$5
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo sqlcmd -S $server -U $username -P $password -d $database -i "$line"
-done < "$5"
+version=$5
+find $version/ -name "*.sql" -print -exec sqlcmd -S $server -U $username -P $password -d $database -i {} \;
