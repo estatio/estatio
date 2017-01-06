3a0d5afe18335fa88273d3f386280354efd2efaa
Dan Haywood <dan@haywood-associates.co.uk>
2017-01-06 10:51:01 +0100

    EST-1029: moves over prod code for financial from estatio-dom to udo-dom-financial

---
+++ udo/financial/dom/src/main/java/org/estatio/dom/financial/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Defines {@link org.estatio.dom.financial.FinancialAccount}s, each of which is
+ * {@link org.estatio.dom.financial.FinancialAccount#getOwner() owned} by a {@link org.estatio.dom.party.Party}.
+ *
+ * <p>
+ * {@link org.estatio.dom.financial.FinancialAccount} is abstract, with 
+ * {@link org.estatio.dom.financial.FinancialAccountType} acts as a powertype.  Currently only a single concrete 
+ * subtype, {@link org.estatio.dom.financial.bankaccount.BankAccount}, exists.
+ * 
+ * <p>
+ * {@link org.estatio.dom.financial.bankaccount.BankAccountType} characterises the type of the bank account, for example
+ * <i>CHECKING</i>.
+ * 
+ * <p>
+ * A {@link org.estatio.dom.party.Party} is required to have a {@link org.estatio.dom.financial.bankaccount.BankAccount}.
+ */
+package org.estatio.dom.financial;
