6cd342ad4ee5dd12041d64cf4cfdd0b6ba31df34
Dan Haywood <dan@haywood-associates.co.uk>
2017-01-13 14:24:26 +0000

    EST-1022: references communications module from the catalog, and deletes the communications module code

--- incode/communications/dom/src/main/java/org/incode/module/communications/dom/impl/commchannel/CommunicationChannelOwner_emailAddressTitles.java
+++
@@ -1,9 +0,0 @@
-package org.incode.module.communications.dom.impl.commchannel;
-
-public abstract class CommunicationChannelOwner_emailAddressTitles extends CommunicationChannelOwner_communicationChannelTitlesAbstract {
-
-    public CommunicationChannelOwner_emailAddressTitles(final CommunicationChannelOwner owner, final String separator) {
-        super(owner, separator, CommunicationChannelType.EMAIL_ADDRESS);
-    }
-
-}
--- incode/communications/dom/src/main/java/org/incode/module/communications/dom/impl/commchannel/CommunicationChannelOwner_phoneNumberTitles.java
+++
@@ -1,9 +0,0 @@
-package org.incode.module.communications.dom.impl.commchannel;
-
-public abstract class CommunicationChannelOwner_phoneNumberTitles extends CommunicationChannelOwner_communicationChannelTitlesAbstract {
-
-    public CommunicationChannelOwner_phoneNumberTitles(final CommunicationChannelOwner owner, final String separator) {
-        super(owner, separator, CommunicationChannelType.PHONE_NUMBER);
-    }
-
-}
