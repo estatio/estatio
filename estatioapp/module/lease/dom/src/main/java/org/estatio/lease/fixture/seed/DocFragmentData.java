c699125c47651cd2cb06061e07aa47464ec5990a
Dan Haywood <dan@haywood-associates.co.uk>
2017-02-03 00:09:02 +0000

    EST-891: wires DocFragmentService in
    new InvoiceDescriptionService, also a DocFragmentSeedService

---
+++ estatioapp/module/lease/dom/src/main/java/org/estatio/lease/fixture/seed/DocFragmentData.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2014 Dan Haywood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.estatio.lease.fixture.seed;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import com.google.common.io.Resources;
+
+import org.apache.commons.io.Charsets;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+import org.incode.module.docfragment.dom.impl.DocFragment;
+import org.incode.module.docfragment.dom.impl.DocFragmentRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum DocFragmentData {
+
+    InvoiceItemDescriptionIta(
+            "org.estatio.dom.lease.invoicing.InvoiceItemForLease", "description", "/ITA",
+            "InvoiceItem_description_ITA.docFragment.txt"),
+    InvoiceItemDescriptionFra(
+            "org.estatio.dom.lease.invoicing.InvoiceItemForLease", "description", "/FRA",
+            "InvoiceItem_description_FRA.docFragment.txt"),
+    ;
+
+    @Getter
+    private final String objectType;
+    @Getter
+    private final String name;
+    @Getter
+    private final String atPath;
+    private final String templateResourceName;
+
+    public String getTemplateText() {
+        return read(templateResourceName);
+    }
+
+    public FixtureScript script() {
+        class DocFragmentScript extends FixtureScript {
+            @Override
+            public void execute(final ExecutionContext executionContext) {
+
+                final DocFragment docFrag = docFragmentRepository
+                        .findByObjectTypeAndNameAndApplicableToAtPath(objectType, name, atPath);
+                if(docFrag != null && Objects.equals(docFrag.getAtPath(), atPath)) {
+                    return;
+                }
+
+                docFragmentRepository.create(objectType, name, atPath, getTemplateText());
+            }
+
+            @Inject
+            DocFragmentRepository docFragmentRepository;
+        }
+        return new DocFragmentScript();
+    }
+
+    public static String read(final String resourceName) {
+        try {
+            return Resources.toString(Resources.getResource(DocFragmentData.class, resourceName), Charsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
