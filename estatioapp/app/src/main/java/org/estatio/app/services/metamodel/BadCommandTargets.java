322d0e1ce8da2919e995fec1a365bb45ab910083
Dan Haywood <dan@haywood-associates.co.uk>
2017-02-17 14:27:39 +0000

    EST-1116: fixes for stricter metamodel validation
    * bump excel, sessionloger and classification modules
    * adds in some missing objectTypes for view models
    * enable 'explicitObjectType' check

--- estatioapp/app/src/main/java/org/estatio/app/services/metamodel/BadCommandTargets.java
+++ estatioapp/app/src/main/java/org/estatio/app/services/metamodel/BadCommandTargets.java
@@ -28,8 +28,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.Nature;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.SemanticsOf;
 import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
@@ -50,6 +52,10 @@ import lombok.Setter;
 )
 public class BadCommandTargets {
 
+    @DomainObject(
+            nature = Nature.VIEW_MODEL,
+            objectType = "org.estatio.app.services.metamodel.BadCommandTargets$BadTarget"
+    )
     @XmlRootElement
     public static class BadTarget {
         @Getter @Setter
