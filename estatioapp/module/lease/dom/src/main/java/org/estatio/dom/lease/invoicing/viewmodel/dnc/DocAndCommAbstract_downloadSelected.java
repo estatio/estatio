43a02d1b26a1bfc85911fbb0dc94462d5177231f
Dan Haywood <dan@haywood-associates.co.uk>
2017-03-17 11:38:41 +0000

    EST-1180: download single PDF for DocAndComm view model (as well as in bulk)

--- estatioapp/module/lease/dom/src/main/java/org/estatio/dom/lease/invoicing/viewmodel/dnc/DocAndCommAbstract_downloadSelected.java
+++ estatioapp/module/lease/dom/src/main/java/org/estatio/dom/lease/invoicing/viewmodel/dnc/DocAndCommAbstract_downloadSelected.java
@@ -27,9 +27,11 @@ import com.google.common.collect.Lists;
 
 import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.Bulk;
 import org.apache.isis.applib.annotation.Contributed;
 import org.apache.isis.applib.annotation.InvokeOn;
 import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.message.MessageService;
 import org.apache.isis.applib.services.scratchpad.Scratchpad;
 import org.apache.isis.applib.value.Blob;
 
@@ -46,44 +48,51 @@ import org.estatio.dom.invoice.DocumentTypeData;
 import static org.incode.module.communications.dom.mixins.DocumentConstants.MIME_TYPE_APPLICATION_PDF;
 
 /**
- * It doesn't seem to be possible to combine this with {@link DocAndCommAbstract_downloadSelected};
- * the bulk action must return a java.lang.Object whereas this must return a Blob.
+ * It doesn't seem to be possible to combine this with {@link DocAndCommAbstract_download};
+ * the regular action must return a Blob whereas this must return a java.lang.Object.
  */
-public abstract class DocAndCommAbstract_download<T extends DocAndCommAbstract<T>> extends DocAndCommAbstract_abstract<T> {
+public abstract class DocAndCommAbstract_downloadSelected<T extends DocAndCommAbstract<T>> extends DocAndCommAbstract_abstract<T> {
 
     final String fileName;
 
-    public DocAndCommAbstract_download(final T docAndComm, final DocumentTypeData documentTypeData) {
+    public DocAndCommAbstract_downloadSelected(final T docAndComm, final DocumentTypeData documentTypeData) {
         super(docAndComm, documentTypeData);
         this.fileName = documentTypeData.getMergedFileName();
     }
 
     @Action(
             semantics = SemanticsOf.SAFE,
-            invokeOn = InvokeOn.OBJECT_ONLY
+            invokeOn = InvokeOn.COLLECTION_ONLY
     )
     @ActionLayout(contributed = Contributed.AS_ACTION)
-    public Blob act() throws IOException {
+    public Object act() throws IOException {
 
-        final List<byte[]> pdfBytes = Lists.newArrayList();
+        final List<byte[]> pdfBytes = createOrLookupPdfBytes();
 
         final Document document = getDocument();
-        appendBytes(document, pdfBytes);
 
-        final List<Document> supportingDocs = attachmentsProvider.attachmentsFor(document);
-        for (Document supportingDoc : supportingDocs) {
-            appendBytes(supportingDoc, pdfBytes);
+        // in a bulk situation it's possible that some DnC's have a document, others do not.
+        // we just ignore those that do not
+        if (document != null) {
+            appendBytes(document, pdfBytes);
+
+            final List<Document> supportingDocs = attachmentsProvider.attachmentsFor(document);
+            for (Document supportingDoc : supportingDocs) {
+                appendBytes(supportingDoc, pdfBytes);
+            }
         }
 
-        final byte[] mergedBytes = pdfBoxService.merge(pdfBytes.toArray(new byte[][] {}));
-        return new Blob(fileName, MIME_TYPE_APPLICATION_PDF, mergedBytes);
-    }
+        if(interactionContext.isLast()) {
+            if(pdfBytes.isEmpty()) {
+                messageService.warnUser("No documents to be merged");
+                return null;
+            }
 
-    public String disableAct() {
-        // guard for when invoked on single DnC; ignored if invoke in bulk
-        return getDocument() == null || getDocument().getState() != DocumentState.RENDERED
-                ? "No document has been prepared & rendered"
-                : null;
+            final byte[] mergedBytes = pdfBoxService.merge(pdfBytes.toArray(new byte[][] {}));
+            return new Blob(fileName, MIME_TYPE_APPLICATION_PDF, mergedBytes);
+        }
+
+        return null;
     }
 
     private static void appendBytes(final Document document, final List<byte[]> pdfBytes) {
@@ -97,6 +106,17 @@ public abstract class DocAndCommAbstract_download<T extends DocAndCommAbstract<T
         pdfBytes.add(bytes);
     }
 
+    private List<byte[]> createOrLookupPdfBytes() {
+        final List<byte[]> pdfBytes ;
+        if(interactionContext.isFirst()) {
+            pdfBytes = Lists.newArrayList();
+            scratchpad.put("pdfBytes", pdfBytes);
+        } else {
+            pdfBytes = (List<byte[]>) scratchpad.get("pdfBytes");
+        }
+        return pdfBytes;
+    }
+
 
     @Inject
     Document_communicationAttachments.Provider attachmentsProvider;
@@ -104,7 +124,14 @@ public abstract class DocAndCommAbstract_download<T extends DocAndCommAbstract<T
     @Inject
     PdfBoxService pdfBoxService;
 
+    // TODO: @ActionInvocationContext is broken...
+    @Inject
+    Bulk.InteractionContext interactionContext;
+
     @Inject
     Scratchpad scratchpad;
+
+    @Inject
+    MessageService messageService;
 
 }
