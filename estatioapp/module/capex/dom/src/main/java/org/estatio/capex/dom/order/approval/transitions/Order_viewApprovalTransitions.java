061d99412a4173b26e1bc413bed1c44fc6aa2ef3
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-04 21:58:52 +0100

    EST-1442: adds Order_viewApprovalTransitions mixin (was missing)

---
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/order/approval/transitions/Order_viewApprovalTransitions.java
@@ -0,0 +1,32 @@
+package org.estatio.capex.dom.order.approval.transitions;
+
+import java.util.List;
+
+import org.apache.isis.applib.annotation.Mixin;
+
+import org.estatio.capex.dom.order.Order;
+import org.estatio.capex.dom.order.approval.OrderApprovalState;
+import org.estatio.capex.dom.order.approval.OrderApprovalStateTransition;
+import org.estatio.capex.dom.order.approval.OrderApprovalStateTransitionType;
+import org.estatio.capex.dom.state.DomainObject_viewTransitionsAbstract;
+
+@Mixin(method = "act")
+public class Order_viewApprovalTransitions
+        extends DomainObject_viewTransitionsAbstract<
+                        Order,
+                        OrderApprovalStateTransition,
+                        OrderApprovalStateTransitionType,
+                        OrderApprovalState> {
+
+    public Order_viewApprovalTransitions(final Order order) {
+        super(order,
+                OrderApprovalStateTransition.class);
+    }
+
+    // necessary because Isis' metamodel unable to infer return type from generic method
+    @Override
+    public List<OrderApprovalStateTransition> act() {
+        return super.act();
+    }
+
+}
