5ee7148d881e3adbcce4e7662c1d99a259a0dde9
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-07 16:42:05 +0100

    EST-1478: prevents invoice from being specified as iban proof if some other iban proof PDF (ie, with a doctype of IBAN_PROOF) has been added.

--- estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/bankaccount/documents/BankAccount_attachInvoiceAsVerificationProof.java
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/bankaccount/documents/BankAccount_attachInvoiceAsVerificationProof.java
@@ -1,6 +1,9 @@
 package org.estatio.capex.dom.bankaccount.documents;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -11,17 +14,20 @@ import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Mixin;
 
 import org.incode.module.document.dom.impl.docs.Document;
+import org.incode.module.document.dom.impl.paperclips.Paperclip;
 import org.incode.module.document.dom.impl.paperclips.PaperclipRepository;
 
 import org.estatio.capex.dom.documents.LookupAttachedPdfService;
 import org.estatio.capex.dom.invoice.IncomingInvoice;
 import org.estatio.capex.dom.invoice.IncomingInvoiceRepository;
 import org.estatio.dom.financial.bankaccount.BankAccount;
+import org.estatio.dom.invoice.DocumentTypeData;
 
 @Mixin(method="act")
 public class BankAccount_attachInvoiceAsVerificationProof {
 
     public static final String ROLE_NAME_FOR_IBAN_PROOF = BankAccount_attachPdfAsVerificationProof.ROLE_NAME_FOR_IBAN_PROOF;
+    public static final Predicate<Paperclip> HAS_IBAN_PROOF_DOCTYPE = paperclip -> DocumentTypeData.IBAN_PROOF.isDocTypeFor(paperclip.getDocument());
 
     private final BankAccount bankAccount;
 
@@ -44,11 +50,32 @@ public class BankAccount_attachInvoiceAsVerificationProof {
                     lookupAttachedPdfService.lookupIncomingInvoicePdfsFrom(invoice);
             documents.addAll(attachedDocuments);
         }
+
+        final List<Document> documentsCurrentlyAttachedAsIbanProof = paperclipRepository
+                .findByAttachedToAndRoleName(bankAccount, ROLE_NAME_FOR_IBAN_PROOF).stream()
+                .map(Paperclip::getDocument)
+                .filter(Document.class::isInstance)
+                .map(Document.class::cast)
+                .collect(Collectors.toList());
+
+        documents.removeAll(documentsCurrentlyAttachedAsIbanProof);
         return documents;
     }
 
     public String disableAct() {
-        return choices0Act().isEmpty() ? "No invoices found for this bank account" : null;
+
+        final Optional<Paperclip> ibanProofDoctypePaperclipIfAny = paperclipRepository
+                .findByAttachedToAndRoleName(bankAccount, ROLE_NAME_FOR_IBAN_PROOF).stream()
+                .filter(HAS_IBAN_PROOF_DOCTYPE).findAny();
+        if(ibanProofDoctypePaperclipIfAny.isPresent()) {
+            return "IBAN proof PDF has already been attached";
+        }
+
+        if (choices0Act().isEmpty()) {
+            return "No (other) invoices found for this bank account";
+        }
+
+        return null;
     }
 
 
