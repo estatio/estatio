c7f1f01f6a3945f77cb3efbd77388025fad70584
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-07 10:02:51 +0100

    EST-1473: adds mixins to lookup bic and update bic for a bank account

---
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/bankaccount/verification/triggers/BankAccount_updateBic.java
@@ -0,0 +1,33 @@
+package org.estatio.capex.dom.bankaccount.verification.triggers;
+
+import java.net.MalformedURLException;
+
+import javax.annotation.Nullable;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+import org.estatio.dom.financial.bankaccount.BankAccount;
+
+@Mixin(method="act")
+public class BankAccount_updateBic {
+
+    private final BankAccount bankAccount;
+
+    public BankAccount_updateBic(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Action(
+            semantics = SemanticsOf.IDEMPOTENT
+    )
+    public BankAccount act(@Nullable String bic) throws MalformedURLException {
+        bankAccount.setBic(bic);
+        return bankAccount;
+    }
+
+    public String default0Act() { return bankAccount.getBic(); }
+
+}
