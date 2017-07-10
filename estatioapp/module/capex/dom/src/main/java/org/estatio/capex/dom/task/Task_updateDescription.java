a659e634a79a41945a2c907f58c41b8df04ab0c4
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-10 12:38:30 +0100

    EST-1390: can now update task description.  Also:
    Also:
    - reworks the Task 'snooze' action, renamed to reprioritize (and can do so relatively)

---
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/task/Task_updateDescription.java
@@ -0,0 +1,30 @@
+package org.estatio.capex.dom.task;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.Contributed;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.SemanticsOf;
+
+@Mixin(method = "act")
+public class Task_updateDescription {
+
+    protected final Task task;
+
+    public Task_updateDescription(final Task task) {
+        this.task = task;
+    }
+
+    @Action(semantics = SemanticsOf.IDEMPOTENT)
+    @ActionLayout(contributed = Contributed.AS_ACTION)
+    public Task act(final String description) {
+        task.setDescription(description);
+        return task;
+    }
+
+    public String default0Act() {
+        return task.getDescription();
+    }
+
+
+}
