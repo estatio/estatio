1c8e528bccaafac3000511f6f384c2f8f072dc48
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-19 14:51:25 +0100

    EST-1459: confirming that a batch has been paid will transition all associated invoices as having been paid also

--- estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/payment/approval/triggers/PaymentBatch_confirmAuthorisation.java
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/payment/approval/triggers/PaymentBatch_confirmAuthorisation.java
@@ -1,14 +1,20 @@
 package org.estatio.capex.dom.payment.approval.triggers;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import org.assertj.core.util.Lists;
 
 import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.Mixin;
 import org.apache.isis.applib.annotation.SemanticsOf;
 
+import org.estatio.capex.dom.invoice.IncomingInvoice;
+import org.estatio.capex.dom.invoice.approval.IncomingInvoiceApprovalStateTransitionType;
 import org.estatio.capex.dom.payment.PaymentBatch;
 import org.estatio.capex.dom.payment.approval.PaymentBatchApprovalStateTransitionType;
+import org.estatio.capex.dom.state.StateTransitionService;
 
 @Mixin(method="act")
 public class PaymentBatch_confirmAuthorisation extends PaymentBatch_triggerAbstract {
@@ -25,12 +31,22 @@ public class PaymentBatch_confirmAuthorisation extends PaymentBatch_triggerAbstr
     public PaymentBatch act(
             @Nullable final String comment) {
         trigger(comment, null);
-        return getDomainObject();
+
+        Lists.newArrayList(paymentBatch.getLines()).forEach(x -> triggerPaidOn(x.getInvoice()));
+
+        return paymentBatch;
+    }
+
+    private void triggerPaidOn(final IncomingInvoice invoice) {
+        stateTransitionService.trigger(invoice, IncomingInvoiceApprovalStateTransitionType.PAY_BY_IBP, null, null);
     }
 
     public boolean hideAct() {
         return cannotTransition();
     }
 
+
+    @Inject
+    StateTransitionService stateTransitionService;
 
 }
