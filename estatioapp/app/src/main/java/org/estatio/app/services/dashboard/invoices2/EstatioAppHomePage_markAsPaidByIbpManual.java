e3ff79663db50c6d00e9a5881dcdb958c6ab7452
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-19 16:08:53 +0100

    EST-1510: adds the ability to pay DD or manual_process in bulk

---
+++ estatioapp/app/src/main/java/org/estatio/app/services/dashboard/invoices2/EstatioAppHomePage_markAsPaidByIbpManual.java
@@ -0,0 +1,54 @@
+package org.estatio.app.services.dashboard.invoices2;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.factory.FactoryService;
+
+import org.estatio.app.services.dashboard.EstatioAppHomePage;
+import org.estatio.capex.dom.invoice.IncomingInvoice;
+import org.estatio.capex.dom.invoice.approval.triggers.IncomingInvoice_markAsPaidByIbpManual;
+
+@Mixin(method = "act")
+public class EstatioAppHomePage_markAsPaidByIbpManual {
+
+    private final EstatioAppHomePage homePage;
+
+    public EstatioAppHomePage_markAsPaidByIbpManual(EstatioAppHomePage homePage) {
+        this.homePage = homePage;
+    }
+
+    @Action(semantics = SemanticsOf.IDEMPOTENT)
+    @ActionLayout(position = ActionLayout.Position.PANEL)
+    public EstatioAppHomePage act(
+            final List<IncomingInvoice> invoices,
+            @Nullable
+            final String comment) {
+
+        for (IncomingInvoice invoice : invoices) {
+            factoryService.mixin(IncomingInvoice_markAsPaidByIbpManual.class, invoice).act(comment);
+        }
+
+        return homePage;
+    }
+    public List<IncomingInvoice> choices0Act() {
+        return homePage.getIncomingInvoicesPayableByManualProcess();
+    }
+    public List<IncomingInvoice> default0Act() {
+        return choices0Act();
+    }
+    public boolean hideAct() {
+        // hide (rather than disable) this action so that, once all these invoices have been dealt with, then the action will disappear.
+        return choices0Act().isEmpty();
+    }
+
+
+    @Inject
+    FactoryService factoryService;
+}
