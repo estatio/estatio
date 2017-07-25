9682ca2d7390117f6a66bd7703a4c9a1ba036f26
Rosco Kalis <roscokalis@gmail.com>
2017-07-25 15:05:41 +0200

    EST-1533: Changed IncomingDocumentRepository::matchAllIncomingDocuments and IncomingDocumentRepository::findAllIncomingDocuments to typesafe queries, changed all other finders from Guava iterable to Java8 streams API. Replaced corresponding tests

---
+++ estatioapp/integtests/src/test/java/org/estatio/integtests/capex/IncomingDocumentRepository_IntegTest.java
@@ -0,0 +1,81 @@
+package org.estatio.integtests.capex;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+import org.incode.module.document.dom.impl.docs.Document;
+
+import org.estatio.capex.dom.documents.IncomingDocumentRepository;
+import org.estatio.fixture.EstatioBaseLineFixture;
+import org.estatio.fixture.asset.PropertyForOxfGb;
+import org.estatio.fixture.documents.incoming.IncomingPdfFixture;
+import org.estatio.fixture.financial.BankAccountForHelloWorldNl;
+import org.estatio.fixture.party.OrganisationForHelloWorldGb;
+import org.estatio.fixture.party.OrganisationForTopModelGb;
+import org.estatio.integtests.EstatioIntegrationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IncomingDocumentRepository_IntegTest extends EstatioIntegrationTest {
+
+    @Inject IncomingDocumentRepository incomingDocumentRepository;
+
+    @Before
+    public void setupData() {
+        runFixtureScript(new FixtureScript() {
+            @Override
+            protected void execute(final ExecutionContext executionContext) {
+                executionContext.executeChild(this, new EstatioBaseLineFixture());
+                executionContext.executeChild(this, new OrganisationForTopModelGb());
+                executionContext.executeChild(this, new OrganisationForHelloWorldGb());
+                executionContext.executeChild(this, new PropertyForOxfGb());
+                executionContext.executeChild(this, new BankAccountForHelloWorldNl());
+                executionContext.executeChild(this, new IncomingPdfFixture());
+            }
+        });
+    }
+
+    public static class MatchIncomingDocumentByName extends IncomingDocumentRepository_IntegTest {
+        @Test
+        public void when_document_name_is_equal_to_search_phrase_return_documents() {
+            // given
+            String searchPhrase = "fakeOrder1.pdf";
+
+            // when
+            List<Document> matchedDocuments = incomingDocumentRepository.matchAllIncomingDocumentsByName(searchPhrase);
+
+            // then
+            assertThat(matchedDocuments).isNotEmpty();
+        }
+
+        @Test
+        public void when_document_name_contains_case_insensitive_search_phrase_return_documents() {
+            // given
+            String searchPhrase = "keorde";
+
+            // when
+            List<Document> matchedDocuments = incomingDocumentRepository.matchAllIncomingDocumentsByName(searchPhrase);
+
+            // then
+            assertThat(matchedDocuments).isNotEmpty();
+        }
+
+        @Test
+        public void when_document_name_does_not_contain_search_phrase_return_no_documents() {
+            // given
+            String searchPhrase = "snakeOrder1.jpg";
+
+            // when
+            List<Document> matchedDocuments = incomingDocumentRepository.matchAllIncomingDocumentsByName(searchPhrase);
+
+            // then
+            assertThat(matchedDocuments).isEmpty();
+        }
+    }
+}
