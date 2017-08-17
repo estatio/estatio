458bc002997088146d5c5803c17cd1b8faa0ae29
Dan Haywood <dan@haywood-associates.co.uk>
2017-08-17 14:04:50 +0100

    EST-1559: inlines mixins for IncomingInvoiceDownloadManager

---
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/invoice/manager/DocumentPreparer.java
@@ -0,0 +1,78 @@
+package org.estatio.capex.dom.invoice.manager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
+
+import org.assertj.core.util.Lists;
+
+import org.incode.module.document.dom.impl.docs.Document;
+
+import org.estatio.capex.dom.pdfmanipulator.ExtractSpec;
+import org.estatio.capex.dom.pdfmanipulator.PdfManipulator;
+import org.estatio.capex.dom.pdfmanipulator.Stamp;
+
+/**
+ * Wraps a document and is able (using a provided {@link PdfManipulator} object) to extract a selection of pages
+ * from each document and to stamp each page with additional info.
+ */
+class DocumentPreparer {
+
+    private final Document document;
+    private final int numFirstPages;
+    private final int numLastPages;
+
+    File tempFile;
+
+    DocumentPreparer(final Document document, final int numFirstPages, final int numLastPages) {
+        this.document = document;
+        this.numFirstPages = numFirstPages;
+        this.numLastPages = numLastPages;
+    }
+
+    String getDocumentName() {
+        return document.getName();
+    }
+
+    DocumentPreparer stampUsing(final PdfManipulator pdfManipulator) {
+        try {
+            final String documentName = document.getName();
+
+            final List<String> leftLineTexts = Lists.newArrayList(documentName);
+            final List<String> rightLineTexts = Collections.emptyList();
+            final String hyperlink = null;
+
+            byte[] bytes = pdfManipulator.extractAndStamp(document.getBlobBytes(),
+                    new ExtractSpec(numFirstPages, numLastPages),
+                    new Stamp(leftLineTexts, rightLineTexts, hyperlink));
+
+            tempFile = File.createTempFile(documentName, "pdf");
+
+            final FileOutputStream fos = new FileOutputStream(tempFile);
+            fos.write(bytes);
+            fos.close();
+
+            return this;
+        } catch (IOException e) {
+            throw new IncomingInvoiceDownloadManager.DownloadException("Failed to prepare: " + document.getName(), e);
+        }
+    }
+
+    File getTempFile() {
+        return tempFile;
+    }
+
+    void cleanup() {
+        if (tempFile != null) {
+            try {
+                Files.delete(tempFile.toPath());
+                tempFile.delete();
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+    }
+}
--- estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/invoice/manager/IncomingInvoiceDownloadManager.java
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/invoice/manager/IncomingInvoiceDownloadManager.java
@@ -1,19 +1,35 @@
 package org.estatio.capex.dom.invoice.manager;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 
+import org.assertj.core.util.Lists;
 import org.joda.time.LocalDate;
 
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.CollectionLayout;
+import org.apache.isis.applib.annotation.Contributed;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.Nature;
+import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.PropertyLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.value.Blob;
+
+import org.isisaddons.module.excel.dom.ExcelService;
+import org.isisaddons.module.excel.dom.WorksheetContent;
+import org.isisaddons.module.excel.dom.WorksheetSpec;
 
 import org.incode.module.document.dom.impl.docs.Document;
 import org.incode.module.document.dom.impl.docs.DocumentAbstract;
@@ -28,6 +44,7 @@ import org.estatio.capex.dom.invoice.IncomingInvoiceRepository;
 import org.estatio.capex.dom.invoice.IncomingInvoiceType;
 import org.estatio.capex.dom.invoice.approval.IncomingInvoiceApprovalState;
 import org.estatio.capex.dom.invoice.approval.IncomingInvoiceApprovalStateTransition;
+import org.estatio.capex.dom.pdfmanipulator.PdfManipulator;
 import org.estatio.capex.dom.state.StateTransitionRepositoryGeneric;
 import org.estatio.dom.asset.Property;
 import org.estatio.dom.asset.PropertyRepository;
@@ -41,6 +58,15 @@ import lombok.Setter;
         objectType = "org.estatio.capex.dom.invoice.manager.IncomingInvoiceDownloadManager")
 @NoArgsConstructor
 public class IncomingInvoiceDownloadManager {
+
+
+    public static class DownloadException extends RuntimeException {
+        public DownloadException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+    }
+
+
 
     public String title() {
         return "Invoice Download";
@@ -89,7 +115,7 @@ public class IncomingInvoiceDownloadManager {
     }
 
     @Programmatic
-    public List<IncomingInvoiceItem> getInvoiceItems() {
+    private List<IncomingInvoiceItem> getInvoiceItems() {
         return getInvoices().stream()
                 .flatMap(inv -> inv.getItems().stream())
                 .map(invoiceItem -> (IncomingInvoiceItem) invoiceItem)
@@ -100,6 +126,174 @@ public class IncomingInvoiceDownloadManager {
     public IncomingInvoiceDownloadManager init() {
         return this;
     }
+
+
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(contributed= Contributed.AS_ACTION)
+    public IncomingInvoiceDownloadManager changeParameters(
+            final LocalDate fromInputDate,
+            final LocalDate toInputDate,
+            @Nullable
+            final org.estatio.dom.asset.Property property,
+            @Nullable
+            final IncomingInvoiceType incomingInvoiceType){
+        setFromInputDate(fromInputDate);
+        setToInputDate(toInputDate);
+        setPropertyReference(property == null ? null : property.getReference());
+        setIncomingInvoiceTypeName(incomingInvoiceType == null ? null : incomingInvoiceType.name());
+        return new IncomingInvoiceDownloadManager(fromInputDate, toInputDate, property, incomingInvoiceType);
+    }
+
+    public LocalDate default0ChangeParameters() {
+        return getFromInputDate();
+    }
+
+    public LocalDate default1ChangeParameters() {
+        return getToInputDate();
+    }
+
+    public Property default2ChangeParameters() {
+        return getProperty();
+    }
+
+    public IncomingInvoiceType default3ChangeParameters() {
+        return getIncomingInvoiceType();
+    }
+
+
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    public Blob downloadToExcel(final String fileName) {
+
+        final List<IncomingInvoiceExport> exports = getInvoiceItems().stream()
+                .map(item -> new IncomingInvoiceExport(
+                        item,
+                        documentNumberFor(item),
+                        codaElementFor(item),
+                        commentsFor(item)))
+                .sorted(Comparator.comparing(x -> x.getDocumentNumber()))
+                .collect(Collectors.toList());
+
+        WorksheetSpec spec = new WorksheetSpec(IncomingInvoiceDownloadManager.exportClass, "invoiceExport");
+        WorksheetContent worksheetContent = new WorksheetContent(exports, spec);
+        return excelService.toExcel(worksheetContent, fileName);
+    }
+
+    public String default0DownloadToExcel() {
+        return defaultFileNameWithSuffix(".xlsx");
+    }
+
+    public String disableDownloadToExcel() {
+        return getInvoices().isEmpty() ? "No invoices to download": null;
+    }
+
+
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(named = "Download single PDF")
+    public Blob downloadToPdfSingle(
+            final String fileName,
+            @ParameterLayout(named = "How many first pages of each invoice's PDF?")
+            final Integer numFirstPages,
+            @ParameterLayout(named = "How many final pages of each invoice's PDF?")
+            final Integer numLastPages) throws IOException {
+
+        final List<DocumentPreparer> preparers = documentPreparersForInvoices(numFirstPages, numLastPages);
+        final List<File> fileList = filesFrom(preparers);
+
+        final byte[] singlePdf = pdfBoxService2.merge(fileList);
+
+        preparers.forEach(DocumentPreparer::cleanup);
+
+        return new Blob(fileName, "application/pdf", singlePdf);
+    }
+
+    public String disableDownloadToPdfSingle() {
+        return getInvoices().isEmpty() ? "No invoices to download": null;
+    }
+
+    public String default0DownloadToPdfSingle() {
+        return defaultFileNameWithSuffix(".pdf");
+    }
+    public Integer default1DownloadToPdfSingle() {
+        return 3;
+    }
+    public List<Integer> choices1DownloadToPdfSingle() {
+        return Lists.newArrayList(1,2,3,4,5,10,20,50);
+    }
+    public Integer default2DownloadToPdfSingle() {
+        return 1;
+    }
+    public List<Integer> choices2DownloadToPdfSingle() {
+        return Lists.newArrayList(0,1,2,3,4,5,10,20,50);
+    }
+
+
+
+    private List<File> filesFrom(final List<DocumentPreparer> preparers) {
+        return preparers.stream()
+                .map(preparer -> preparer.stampUsing(pdfManipulator).getTempFile())
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+
+
+
+    private List<DocumentPreparer> documentPreparersForInvoices() {
+        return documentPreparersForInvoices(Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+
+    private List<DocumentPreparer> documentPreparersForInvoices(
+            Integer numFirstPages,
+            Integer numLastPages) {
+        return getInvoices().stream()
+                .map(incomingInvoice -> lookupAttachedPdfService.lookupIncomingInvoicePdfFrom(incomingInvoice))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .sorted(Comparator.comparing(DocumentAbstract::getName))
+                .map(document -> new DocumentPreparer(document, numFirstPages, numLastPages))
+                .collect(Collectors.toList());
+    }
+
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(named = "Download all PDFs (zipped)")
+    public Blob downloadToPdfZipped(final String fileName) throws IOException {
+
+        final List<DocumentPreparer> preparers = documentPreparersForInvoices();
+        final List<ZipService.FileAndName> fileList = fileAndNamesFrom(preparers);
+
+        final byte[] zipBytes = zipService.zip(fileList);
+
+        preparers.forEach(DocumentPreparer::cleanup);
+
+        return new Blob(fileName, "application/zip", zipBytes);
+    }
+
+    public String disableDownloadToPdfZipped() {
+        return getInvoices().isEmpty() ? "No invoices to download": null;
+    }
+
+    public String default0DownloadToPdfZipped() {
+        return defaultFileNameWithSuffix(".zip");
+    }
+
+
+
+    private List<ZipService.FileAndName> fileAndNamesFrom(final List<DocumentPreparer> preparers) {
+        return preparers.stream()
+                .map(preparer -> new ZipService.FileAndName(preparer.getDocumentName(), preparer.stampUsing(pdfManipulator).getTempFile()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+
+
 
 
     CodaElement codaElementFor(final IncomingInvoiceItem x) {
@@ -145,9 +339,6 @@ public class IncomingInvoiceDownloadManager {
     @javax.inject.Inject
     IncomingInvoiceRepository incomingInvoiceRepository;
 
-    @javax.inject.Inject
-    LookupAttachedPdfService lookupAttachedPdfService;
-
     @Inject
     CodaMappingRepository codaMappingRepository;
 
@@ -155,8 +346,25 @@ public class IncomingInvoiceDownloadManager {
     PropertyRepository propertyRepository;
 
     @Inject
-    protected StateTransitionRepositoryGeneric stateTransitionRepositoryGeneric;
+    StateTransitionRepositoryGeneric stateTransitionRepositoryGeneric;
 
+
+    @javax.inject.Inject
+    ExcelService excelService;
+
+
+    @javax.inject.Inject
+    PdfManipulator pdfManipulator;
+
+    @javax.inject.Inject
+    LookupAttachedPdfService lookupAttachedPdfService;
+
+
+    @javax.inject.Inject
+    PdfBoxService2 pdfBoxService2;
+
+    @javax.inject.Inject
+    ZipService zipService;
 
 
 }
