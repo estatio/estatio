b8fa25f1b70f0712c9beddcc134e8f9a26f836a8
Dan Haywood <dan@haywood-associates.co.uk>
2017-08-24 12:01:14 +0100

    EST-1575: adds derived properties to OrderItemInvoiceItemLink

--- estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/orderinvoice/OrderItemInvoiceItemLink.java
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/orderinvoice/OrderItemInvoiceItemLink.java
@@ -1,5 +1,8 @@
 package org.estatio.capex.dom.orderinvoice;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import javax.inject.Inject;
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdGeneratorStrategy;
@@ -8,12 +11,24 @@ import javax.jdo.annotations.Query;
 import javax.jdo.annotations.Unique;
 import javax.jdo.annotations.VersionStrategy;
 
+import org.joda.time.LocalDate;
+
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.annotation.Where;
 import org.apache.isis.applib.services.repository.RepositoryService;
 
 import org.estatio.capex.dom.invoice.IncomingInvoiceItem;
+import org.estatio.capex.dom.invoice.IncomingInvoiceType;
+import org.estatio.capex.dom.order.Order;
 import org.estatio.capex.dom.order.OrderItem;
+import org.estatio.capex.dom.project.Project;
+import org.estatio.dom.asset.Property;
+import org.estatio.dom.budgeting.budgetitem.BudgetItem;
+import org.estatio.dom.charge.Charge;
+import org.estatio.dom.invoice.Invoice;
+import org.estatio.dom.tax.Tax;
+import org.estatio.dom.tax.TaxRate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -61,10 +76,114 @@ public class OrderItemInvoiceItemLink {
     @Column(allowsNull = "false", name = "invoiceItemId")
     private IncomingInvoiceItem invoiceItem;
 
+
+
+    // both order item and invoice item will have the same value for these dimensions
+    public Charge getCharge() {
+        return orderItem.getCharge();
+    }
+    public Property getProperty() {
+        return orderItem.getProperty();
+    }
+    public Project getProject() {
+        return orderItem.getProject();
+    }
+    public BudgetItem getBudgetItem() {
+        return orderItem.getBudgetItem();
+    }
+
+
+
+    // derived from the order item
+    public Order getOrder() {
+        return orderItem.getOrdr();
+    }
+    public String getOrderItemDescription() {
+        return orderItem.getDescription();
+    }
+    public LocalDate getOrderItemStartDate() {
+        return orderItem.getStartDate();
+    }
+    public LocalDate getOrderItemEndDate() {
+        return orderItem.getEndDate();
+    }
+    public BigDecimal getOrderItemNetAmount() {
+        return orderItem.getNetAmount();
+    }
+    public BigDecimal getOrderItemVatAmount() {
+        return orderItem.getVatAmount();
+    }
+    public BigDecimal getOrderItemGrossAmount() {
+        return orderItem.getGrossAmount();
+    }
+    public Tax getOrderItemTax() {
+        return orderItem.getTax();
+    }
+
+
+    // derived from the invoice item
+    public Invoice getInvoice() {
+        return invoiceItem.getInvoice();
+    }
+
+    public IncomingInvoiceType getIncomingInvoiceType() {
+        return invoiceItem.getIncomingInvoiceType();
+    }
+
+    public String getInvoiceItemDescription() {
+        return invoiceItem.getDescription();
+    }
+
+    public BigInteger getInvoiceItemSequence() {
+        return invoiceItem.getSequence();
+    }
+
+    public LocalDate getInvoiceItemDueDate() {
+        return invoiceItem.getDueDate();
+    }
+
+    @org.apache.isis.applib.annotation.Property(hidden = Where.PARENTED_TABLES)
+    public LocalDate getInvoiceItemStartDate() {
+        return invoiceItem.getStartDate();
+    }
+
+    @org.apache.isis.applib.annotation.Property(hidden = Where.PARENTED_TABLES)
+    public LocalDate getInvoiceItemEndDate() {
+        return invoiceItem.getEndDate();
+    }
+
+    public BigDecimal getInvoiceItemNetAmount() {
+        return invoiceItem.getNetAmount();
+    }
+
+    public BigDecimal getInvoiceItemVatAmount() {
+        return invoiceItem.getVatAmount();
+    }
+
+    public BigDecimal getInvoiceItemVatPercentage() {
+        return invoiceItem.getVatPercentage();
+    }
+
+    public BigDecimal getInvoiceItemGrossAmount() {
+        return invoiceItem.getGrossAmount();
+    }
+
+    public Tax getInvoiceItemTax() {
+        return invoiceItem.getTax();
+    }
+
+    public TaxRate getInvoiceItemTaxRate() {
+        return invoiceItem.getTaxRate();
+    }
+
+
+
     @Programmatic
     public void remove() {
         repositoryService.removeAndFlush(this);
     }
+
+
 
     @Inject
     private RepositoryService repositoryService;
