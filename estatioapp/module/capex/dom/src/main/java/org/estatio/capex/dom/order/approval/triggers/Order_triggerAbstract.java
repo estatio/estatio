43a248646283a4b3a8e48c003e533238ae7a9038
Dan Haywood <dan@haywood-associates.co.uk>
2017-09-04 11:46:44 +0100

    EST-1601: copies IncomingInvoice's next and previous mixins, similar ones for Order.
    Also copies supporting methods in the Xxx_triggerAbstract superclass

--- estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/order/approval/triggers/Order_triggerAbstract.java
+++ estatioapp/module/capex/dom/src/main/java/org/estatio/capex/dom/order/approval/triggers/Order_triggerAbstract.java
@@ -2,14 +2,16 @@ package org.estatio.capex.dom.order.approval.triggers;
 
 import java.util.List;
 
-import javax.annotation.Nullable;
+import javax.inject.Inject;
 
-import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.services.queryresultscache.QueryResultsCache;
 
 import org.estatio.capex.dom.order.Order;
 import org.estatio.capex.dom.order.approval.OrderApprovalState;
 import org.estatio.capex.dom.order.approval.OrderApprovalStateTransition;
 import org.estatio.capex.dom.order.approval.OrderApprovalStateTransitionType;
+import org.estatio.capex.dom.task.Task;
+import org.estatio.capex.dom.task.TaskRepository;
 import org.estatio.capex.dom.triggers.DomainObject_triggerAbstract;
 
 public abstract class Order_triggerAbstract
@@ -31,6 +33,97 @@ public abstract class Order_triggerAbstract
             final OrderApprovalStateTransitionType requiredTransitionType) {
         super(order, OrderApprovalStateTransition.class, requiredTransitionType.getFromStates(), requiredTransitionType);
     }
+
+
+    protected Order nextOrderAfterPendingIfRequested(final boolean goToNext) {
+        final Order nextOrder = goToNext ? nextOrderAfterPending() : null;
+        return coalesce(nextOrder, getDomainObject());
+    }
+
+    protected Order nextOrderAfterPending() {
+        return queryResultsCache.execute(
+                this::doNextOrderAfterPending, getClass(), "nextOrderAfterPending", getDomainObject());
+    }
+
+    private Order doNextOrderAfterPending() {
+        final OrderApprovalStateTransition pendingTransition = stateTransitionService
+                .pendingTransitionOf(getDomainObject(), OrderApprovalStateTransition.class);
+        final Order nextOrder = nextOrderViaTask(pendingTransition);
+        return coalesce(nextOrder, getDomainObject());
+    }
+
+    private Order nextOrderViaTask(final OrderApprovalStateTransition transition) {
+        if(transition == null) {
+            return null;
+        }
+        final Task task = transition.getTask();
+        if (task == null) {
+            return null;
+        }
+
+        final Task nextTask = taskRepository.nextTaskAfter(task);
+        if(nextTask == null) {
+            return null;
+        }
+        final OrderApprovalStateTransition nextTransition = stateTransitionRepository.findByTask(nextTask);
+        if(nextTransition == null) {
+            return null;
+        }
+        return nextTransition.getDomainObject();
+    }
+
+
+    protected Order previousOrderBeforePending() {
+        return queryResultsCache.execute(
+                this::doPreviousBeforePending, getClass(), "previousOrderBeforePending", getDomainObject());
+    }
+
+    private Order doPreviousBeforePending() {
+        final OrderApprovalStateTransition pendingTransition = stateTransitionService
+                .pendingTransitionOf(getDomainObject(), OrderApprovalStateTransition.class);
+        final Order previousOrder = previousOrderViaTask(pendingTransition);
+        return coalesce(previousOrder, getDomainObject());
+    }
+
+    private Order previousOrderViaTask(final OrderApprovalStateTransition transition) {
+        if(transition == null) {
+            return null;
+        }
+        final Task task = transition.getTask();
+        if (task == null) {
+            return null;
+        }
+
+        final Task previousTask = taskRepository.previousTaskBefore(task);
+        if(previousTask == null) {
+            return null;
+        }
+        final OrderApprovalStateTransition previousTransition = stateTransitionRepository.findByTask(previousTask);
+        if(previousTransition == null) {
+            return null;
+        }
+        return previousTransition.getDomainObject();
+    }
+
+    private static <T> T coalesce(final T... candidates) {
+        for (T candidate : candidates) {
+            if(candidate != null) {
+                return candidate;
+            }
+        }
+        return null;
+    }
+
+
+
+    @Inject
+    QueryResultsCache queryResultsCache;
+
+    @Inject
+    TaskRepository taskRepository;
+
+    @Inject
+    OrderApprovalStateTransition.Repository stateTransitionRepository;
 
 
 }
