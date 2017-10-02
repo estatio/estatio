4fd48f392453608d945001c762d5b313b286d198
Dan Haywood <dan@haywood-associates.co.uk>
2017-10-02 08:24:26 +0100

    EST-1633: adds missing modules to EstatioDomManifest (as used by isis-validate

--- estatioapp/app/src/main/java/org/estatio/dom/EstatioDomManifest.java
+++ estatioapp/app/src/main/java/org/estatio/dom/EstatioDomManifest.java
@@ -10,8 +10,10 @@ import com.google.common.collect.Maps;
 import org.apache.isis.applib.AppManifest;
 import org.apache.isis.applib.fixturescripts.FixtureScript;
 
+import org.isisaddons.module.excel.ExcelModule;
 import org.isisaddons.module.security.SecurityModule;
 
+import org.estatio.capex.EstatioCapexModule;
 import org.estatio.domsettings.EstatioSettingsDomModule;
 
 public class EstatioDomManifest implements AppManifest {
@@ -19,9 +21,11 @@ public class EstatioDomManifest implements AppManifest {
     @Override
     public List<Class<?>> getModules() {
         List<Class<?>> modules = Lists.newArrayList();
-        modules.add(EstatioDomainModule.class);
-        modules.add(EstatioSettingsDomModule.class);
+        modules.add(EstatioDomainModule.class); // org.estatio.dom
+        modules.add(EstatioSettingsDomModule.class); // org.estatio.domsettings
+        modules.add(EstatioCapexModule.class); // org.estatio.capex
         modules.add(SecurityModule.class);
+        modules.add(ExcelModule.class);
         return modules;
     }
 
