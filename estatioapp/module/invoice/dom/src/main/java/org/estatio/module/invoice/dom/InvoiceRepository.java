20626b217e211482b18c56dec643965e978f9df0
Dan Haywood <dan@haywood-associates.co.uk>
2017-11-07 15:34:05 +0000

    EST-1103: removes unused import of QInvoice

--- estatioapp/module/invoice/dom/src/main/java/org/estatio/module/invoice/dom/InvoiceRepository.java
+++ estatioapp/module/invoice/dom/src/main/java/org/estatio/module/invoice/dom/InvoiceRepository.java
@@ -30,7 +30,6 @@ import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 import org.incode.module.communications.dom.impl.commchannel.CommunicationChannel;
 
 import org.estatio.dom.UdoDomainRepositoryAndFactory;
-import org.estatio.dom.invoice.QInvoice;
 import org.estatio.module.party.dom.Party;
 
 @DomainService(nature = NatureOfService.DOMAIN, repositoryFor = Invoice.class)
