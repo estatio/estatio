9f10836de146c84eaac0ca8ac652cf746b595fdd
Dan Haywood <dan@haywood-associates.co.uk>
2017-12-20 18:03:06 +0000

    ECP-465: moves the source code all into estatioapp.
    The code still compiles, integration tests failing because need to convert
    to use IntegrationTestAbstract3 (I hope)

--- enhance.sh
+++ enhance.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 mvn datanucleus:enhance -pl estatioapp/app -DmetadataDirectory="target/classes" -o
 mvn datanucleus:enhance -pl estatioapp/app -DmetadataDirectory="target/test-classes" -o
-mvn datanucleus:enhance -pl estatioapp/dom/alias/impl -DmetadataDirectory="target/classes" -o
-mvn datanucleus:enhance -pl estatioapp/dom/alias/integtests -DmetadataDirectory="target/test-classes" -o
