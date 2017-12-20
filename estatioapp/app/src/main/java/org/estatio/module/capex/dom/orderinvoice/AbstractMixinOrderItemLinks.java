5c5b4e06e5aece38cf4e0e2663e48655c0642d77
Jeroen van der Wal <jeroen@stromboli.it>
2017-12-20 09:26:30 +0100

    EST-1764: Allows orders the have a negative amount

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/orderinvoice/AbstractMixinOrderItemLinks.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/orderinvoice/AbstractMixinOrderItemLinks.java
@@ -23,17 +23,26 @@ abstract class AbstractMixinOrderItemLinks<T> {
 
         if(proposedNetAmount == null) return null; // shouldn't occur, I think.
 
-        if(proposedNetAmount.compareTo(BigDecimal.ZERO) <= 0) return "Must be a positive amount";
+        if(proposedNetAmount.compareTo(BigDecimal.ZERO) == 0) return "Cannot be zero";
 
         final BigDecimal netAmountAvailableToLink =
                 orderItemInvoiceItemLinkRepository.calculateNetAmountNotLinkedFromInvoiceItem(invoiceItem);
         final BigDecimal netAmountAvailableTakingCurrentIntoAccount =
                 netAmountAvailableToLink.add(currentNetAmount);
 
-        if(proposedNetAmount.compareTo(netAmountAvailableTakingCurrentIntoAccount) > 0) {
-            return "Cannot exceed remaining amount to be linked (" + netAmountAvailableTakingCurrentIntoAccount + ")";
-        }
 
+        // A new requirement to insert orders with negative amouts adds some complexity
+
+        if (netAmountAvailableTakingCurrentIntoAccount.signum() >= 0){
+            //it's a positve number
+            if(proposedNetAmount.compareTo(netAmountAvailableTakingCurrentIntoAccount) > 0) {
+                return "Cannot exceed remaining amount to be linked (" + netAmountAvailableTakingCurrentIntoAccount + ")";
+            }
+        } else {
+            if(proposedNetAmount.compareTo(netAmountAvailableTakingCurrentIntoAccount) < 0) {
+                return "Cannot exceed remaining amount to be linked (" + netAmountAvailableTakingCurrentIntoAccount + ")";
+            }
+        }
         return null;
     }
 
