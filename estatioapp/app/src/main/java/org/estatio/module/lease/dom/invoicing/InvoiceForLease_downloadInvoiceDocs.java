7de62cea51214615ac6ee6cadc20257d055d9210
Johan Doornenbal <johan@filternet.nl>
2018-06-25 17:43:46 +0200

    EST-1802: bug fix

--- estatioapp/app/src/main/java/org/estatio/module/lease/dom/invoicing/InvoiceForLease_downloadInvoiceDocs.java
+++ estatioapp/app/src/main/java/org/estatio/module/lease/dom/invoicing/InvoiceForLease_downloadInvoiceDocs.java
@@ -3,13 +3,13 @@ package org.estatio.module.lease.dom.invoicing;
 import org.apache.isis.applib.annotation.Mixin;
 
 import org.estatio.module.invoice.dom.DocumentTypeData;
-import org.estatio.module.lease.dom.invoicing.summary.comms.DocAndCommForPrelimLetter;
+import org.estatio.module.lease.dom.invoicing.summary.comms.DocAndCommForInvoiceDoc;
 
 @Mixin(method= "act")
 public class InvoiceForLease_downloadInvoiceDocs extends InvoiceForLease_downloadAbstract {
 
     public InvoiceForLease_downloadInvoiceDocs(final InvoiceForLease invoice){
-        super(invoice, i->new DocAndCommForPrelimLetter(i), DocumentTypeData.INVOICE);
+        super(invoice, i->new DocAndCommForInvoiceDoc(i), DocumentTypeData.INVOICE);
     }
 
 }
