e616f541ce0a3bc3759317f7e35908a5cf285439
Sander Ginn <sander@ginn.it>
2018-11-27 16:30:25 +0100

    EST-1892: fixes typo in stream

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoiceRepository.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoiceRepository.java
@@ -154,7 +154,7 @@ public class IncomingInvoiceRepository {
                 .filter(incomingInvoice -> incomingInvoice.getAtPath().startsWith(atPath))
                 .filter(incomingInvoice -> invoiceRepository.findBySeller(incomingInvoice.getSeller())
                         .stream()
-                        .anyMatch(invoice -> invoice.getPaymentMethod() != PaymentMethod.BANK_TRANSFER && incomingInvoice.getPaymentMethod() != PaymentMethod.REFUND_BY_SUPPLIER))
+                        .anyMatch(invoice -> invoice.getPaymentMethod() != PaymentMethod.BANK_TRANSFER && invoice.getPaymentMethod() != PaymentMethod.REFUND_BY_SUPPLIER))
                 .collect(Collectors.toList());
     }
 
