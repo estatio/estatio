8ddbb1d331548439aed2cb2ceb3e42d528be96c4
danhaywood <dan@haywood-associates.co.uk>
2018-12-14 11:44:58 +0000

    ECP-889: adds menu item to lookup CodaDocHead.

---
+++ estatioapp/app/src/main/java/org/estatio/module/coda/dom/doc/CodaDocHeadMenu.java
@@ -0,0 +1,61 @@
+package org.estatio.module.coda.dom.doc;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.message.MessageService;
+
+import org.estatio.module.application.app.CodaCmpCodeService;
+import org.estatio.module.application.app.CodaDocCodeService;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "coda.CodaDocHeadMenu"
+)
+@DomainServiceLayout(
+        menuBar = DomainServiceLayout.MenuBar.PRIMARY,
+        named = "Invoices In",
+        menuOrder = "65.4"
+)
+public class CodaDocHeadMenu {
+
+    @Action(semantics = SemanticsOf.SAFE)
+    public CodaDocHead lookup(
+            final String cmpCode,
+            final String docCode,
+            final String docNum
+    ) {
+        final CodaDocHead docHead =
+                codaDocHeadRepository.findByCmpCodeAndDocCodeAndDocNum(cmpCode, docCode, docNum);
+        if(docHead == null) {
+            messageService.informUser(String.format(
+                    "No Coda document found for '%s | %s | %s'",
+                    cmpCode, docCode, docNum));
+        }
+        return docHead;
+    }
+    public List<String> choices0Lookup() {
+        return codaCmpCodeService.listAll();
+    }
+    public List<String> choices1Lookup() {
+        return codaDocCodeService.listAll();
+    }
+
+
+    @Inject
+    CodaDocHeadRepository codaDocHeadRepository;
+    @Inject
+    MessageService messageService;
+
+    @Inject
+    CodaCmpCodeService codaCmpCodeService;
+    @Inject
+    CodaDocCodeService codaDocCodeService;
+
+}
