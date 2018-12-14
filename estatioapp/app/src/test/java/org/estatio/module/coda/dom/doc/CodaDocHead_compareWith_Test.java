84262057cec493962f7c77820b4e735f3c3e0456
danhaywood <dan@haywood-associates.co.uk>
2018-12-14 17:58:10 +0000

    ECP-888: fix for unit test

--- estatioapp/app/src/test/java/org/estatio/module/coda/dom/doc/CodaDocHead_compareWith_Test.java
+++ estatioapp/app/src/test/java/org/estatio/module/coda/dom/doc/CodaDocHead_compareWith_Test.java
@@ -29,13 +29,13 @@ public class CodaDocHead_compareWith_Test {
     @Before
     public void setUp() throws Exception {
 
-        existing = new CodaDocHead("IT01", "FR-GEN", "1234", (short)1, null, null, null, null, "SHA256", "");
+        existing = new CodaDocHead("IT01", "FR-GEN", "1234", (short)1, null, null, null, null, "SHA256_1", "");
 
         existingSummaryDocLine = addLine(existing, 1, LineType.SUMMARY);
         existingAnalysisDocLine = addLine(existing, 2, LineType.ANALYSIS);
 
 
-        codaDocHead = new CodaDocHead("IT01", "FR-GEN", "1234", (short)2, null, null, null, null, "SHA256", "");
+        codaDocHead = new CodaDocHead("IT01", "FR-GEN", "1234", (short)2, null, null, null, null, "SHA256_2", "");
 
         summaryDocLine = addLine(codaDocHead, 1, LineType.SUMMARY);
         analysisDocLine = addLine(codaDocHead, 2, LineType.ANALYSIS);
@@ -67,9 +67,9 @@ public class CodaDocHead_compareWith_Test {
     }
 
     @Test
-    public void when_differs_but_same_by_timestamp() throws Exception {
+    public void when_differs_but_same_by_sha256() throws Exception {
         // given
-        existing.setCodaTimeStamp((short)2);
+        existing.setSha256(codaDocHead.getSha256());
 
         // when
         CodaDocHead.Comparison comparison = codaDocHead.compareWith(existing);
