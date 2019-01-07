bd405c381f57865699e3a1a061425c338103a4ce
Johan Doornenbal <johan@filternet.nl>
2019-01-07 15:42:31 +0100

    Revert "EST-1917: adds flush transaction when maintaining service charges"
    This reverts commit e92bb9015457e1b8d8642f3841d35d070c801729.

--- estatioapp/app/src/main/java/org/estatio/module/budgetassignment/imports/LeaseTermForServiceChargeBudgetAuditManager.java
+++ estatioapp/app/src/main/java/org/estatio/module/budgetassignment/imports/LeaseTermForServiceChargeBudgetAuditManager.java
@@ -37,7 +37,6 @@ import org.apache.isis.applib.annotation.Parameter;
 import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.annotation.Publishing;
 import org.apache.isis.applib.annotation.SemanticsOf;
-import org.apache.isis.applib.services.xactn.TransactionService;
 import org.apache.isis.applib.value.Blob;
 
 import org.isisaddons.module.excel.dom.ExcelService;
@@ -143,7 +142,6 @@ public class LeaseTermForServiceChargeBudgetAuditManager  {
             if (nextLeaseTerm != null && nextLeaseTerm == nextLeaseTermUploaded) {
                 nextLeaseTerm.setBudgetedValue(lineItem.getNextBudgetedValue());
             }
-            transactionService.flushTransaction();
         }
         return this;
     }
@@ -176,9 +174,6 @@ public class LeaseTermForServiceChargeBudgetAuditManager  {
 
     @Inject
     private LeaseRepository leaseRepository;
-
-    @Inject
-    private TransactionService transactionService;
     //endregion
 
 }
