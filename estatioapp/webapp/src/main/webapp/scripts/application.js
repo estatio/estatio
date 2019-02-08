8527f64632057bdf7617d9ca4012cb72e870e574
danhaywood <dan@haywood-associates.co.uk>
2019-02-08 08:55:29 +0000

    EST-1320: adds static resources to OS version of webapp so that it runs ok

--- estatioapp/webapp/src/main/webapp/scripts/application.js
+++ estatioapp/webapp/src/main/webapp/scripts/application.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	/// here...
+
+    Wicket.Event.subscribe(Isis.Topic.FOCUS_FIRST_PARAMETER, function(jqEvent, elementId) {
+        setTimeout(function() {
+            $(".IncomingInvoice-pendingApprovalTask .inlinePromptForm .actionParametersForm div.parameter .String-complete-0                .scalarValueInput input").prop("readonly", true);
+            $(".IncomingInvoice-pendingApprovalTask .inlinePromptForm .actionParametersForm div.parameter .String-approve-0                 .scalarValueInput input").prop("readonly", true);
+            $(".Task-object                         .inlinePromptForm .actionParametersForm div.parameter .String-completeIncomingInvoice-0 .scalarValueInput input").prop("readonly", true);
+            $(".Task-object                         .inlinePromptForm .actionParametersForm div.parameter .String-approveIncomingInvoice-0  .scalarValueInput input").prop("readonly", true);
+        }, 0);
+    });
+
+/*
+    not actually needed because this is the default (for Chrome, at least)
+
+    $('a.entityUrlSource').bind('click', function(e) {
+       if (e.ctrlKey){
+         e.preventDefault();
+         var url = $(this).attr('href');
+         window.open(url,'_blank')
+       }
+    });
+*/
+
 });
