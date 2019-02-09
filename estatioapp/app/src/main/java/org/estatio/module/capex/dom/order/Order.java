00b116b9006f39a37defa3dafaaac05c2b7a4397
danhaywood <dan@haywood-associates.co.uk>
2019-02-09 13:08:11 +0000

    ECP-927: converts Order#attributes to be a dependent collection (cascade delete)

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/order/Order.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/order/Order.java
@@ -1462,7 +1462,7 @@ public class Order extends UdoDomainObject2<Order> implements Stateful {
         return this;
     }
 
-    @Persistent(mappedBy = "ordr", dependentElement = "false")
+    @Persistent(mappedBy = "ordr", dependentElement = "true")
     @Getter @Setter
     private SortedSet<OrderAttribute> attributes = new TreeSet<OrderAttribute>();
 
