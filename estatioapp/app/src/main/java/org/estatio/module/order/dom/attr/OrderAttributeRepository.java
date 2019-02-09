99fed8ea148e8f987c45b2cf70187a08929da492
danhaywood <dan@haywood-associates.co.uk>
2019-02-09 12:07:38 +0000

    ECP-927:  makes OrderAttributeRepository in sync with InvoiceAttributeRepository
    adds findValueByOrderAndName and findIsOverriddenByOrderAndName

--- estatioapp/app/src/main/java/org/estatio/module/order/dom/attr/OrderAttributeRepository.java
+++ estatioapp/app/src/main/java/org/estatio/module/order/dom/attr/OrderAttributeRepository.java
@@ -51,6 +51,18 @@ public class OrderAttributeRepository extends UdoDomainRepositoryAndFactory<Orde
     }
 
     @Programmatic
+    public String findValueByOrderAndName(final OrderAttributeName orderAttributeName, final Order order) {
+        final OrderAttribute orderAttribute = findByOrderAndName(order, orderAttributeName);
+        return orderAttribute == null ? null : orderAttribute.getValue();
+    }
+
+    @Programmatic
+    public boolean findIsOverriddenByOrderAndName(final OrderAttributeName orderAttributeName, final Order order) {
+        final OrderAttribute orderAttribute = findByOrderAndName(order, orderAttributeName);
+        return orderAttribute != null && orderAttribute.isOverridden();
+    }
+
+    @Programmatic
     public OrderAttribute newAttribute(
             final Order order,
             final OrderAttributeName name,
