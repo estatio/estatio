21c2b571ee77eff794519f1dd1ba85cfc066e4d1
Johan Doornenbal <johan@filternet.nl>
2019-03-04 18:53:30 +0100

    ECP-779: now displays paid amount on project and project item

---
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/contributions/Project_PaidAmount.java
@@ -0,0 +1,62 @@
+package org.estatio.module.capex.dom.invoice.contributions;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.function.Function;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.Column;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.Contributed;
+import org.apache.isis.applib.annotation.Mixin;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.wrapper.WrapperFactory;
+
+import org.estatio.module.capex.dom.invoice.IncomingInvoice;
+import org.estatio.module.capex.dom.invoice.IncomingInvoiceItem;
+import org.estatio.module.capex.dom.invoice.IncomingInvoiceItemRepository;
+import org.estatio.module.capex.dom.project.Project;
+
+/**
+ * TODO: although this could currently be inlined, we expect to factor out project from incoming invoice, in which case this will be a typical contribution across modules.
+ */
+@Mixin
+public class Project_PaidAmount {
+
+    private final Project project;
+    public Project_PaidAmount(Project project){
+        this.project = project;
+    }
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(contributed = Contributed.AS_ASSOCIATION)
+    @Column(scale = 2)
+    public BigDecimal paidAmount(){
+        return project.isParentProject() ? amountWhenParentProject() : sum(IncomingInvoice::getNetAmount);
+    }
+
+    private BigDecimal sum(final Function<IncomingInvoice, BigDecimal> x) {
+        return incomingInvoiceItemRepository.findByProject(project).stream()
+                .filter(i->i.getClass().isAssignableFrom(IncomingInvoiceItem.class))
+                .map(i->(IncomingInvoice) i.getInvoice())
+                .filter(i->i.getPaidDate()!=null)
+                .map(x)
+                .filter(Objects::nonNull)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal amountWhenParentProject(){
+        BigDecimal result = BigDecimal.ZERO;
+        for (Project child : project.getChildren()){
+            result = result.add(wrapperFactory.wrap(new Project_PaidAmount(child)).paidAmount());
+        }
+        return result;
+    }
+
+    @Inject
+    IncomingInvoiceItemRepository incomingInvoiceItemRepository;
+
+    @Inject WrapperFactory wrapperFactory;
+}
