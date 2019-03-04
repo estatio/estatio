be8a51af240ba44a771bca9a56ff701b8b400b47
Johan Doornenbal <johan@filternet.nl>
2019-03-04 16:40:17 +0100

    ECP-949: repairs integration test

--- estatioapp/app/src/test/java/org/estatio/module/capex/integtests/project/ProjectItemTermRepository_IntegTest.java
+++ estatioapp/app/src/test/java/org/estatio/module/capex/integtests/project/ProjectItemTermRepository_IntegTest.java
@@ -34,6 +34,7 @@ import org.estatio.module.capex.dom.project.ProjectItemTermRepository;
 import org.estatio.module.capex.dom.project.Project;
 import org.estatio.module.capex.fixtures.project.enums.Project_enum;
 import org.estatio.module.capex.integtests.CapexModuleIntegTestAbstract;
+import org.estatio.module.charge.fixtures.charges.enums.Charge_enum;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -53,14 +54,17 @@ public class ProjectItemTermRepository_IntegTest extends CapexModuleIntegTestAbs
     public void create_project_term_works() throws Exception {
 
         // given
+        final LocalDate itemStartDate = new LocalDate(2018, 1, 1);
+        final LocalDate itemEndDate = new LocalDate(2018, 3, 31);
         Project projectForKal = Project_enum.KalProject1.findUsing(serviceRegistry);
+        projectForKal.addItem(Charge_enum.NlIncomingCharge1.findUsing(serviceRegistry), "Some item", new BigDecimal("150000.00"),  itemStartDate, itemEndDate,null, null);
         final ProjectItem firstItem = projectForKal.getItems().first();
         assertThat(firstItem.getProjectItemTerms()).isEmpty();
 
         // when
-        final BigDecimal amount = new BigDecimal("10000.00");
         final LocalDate startDate = new LocalDate(2018, 1, 1);
         final LocalDate endDate = new LocalDate(2018, 3, 31);
+        final BigDecimal amount = new BigDecimal("10000.00");
         firstItem.newProjectItemTerm(amount, startDate, endDate);
 
         // then
@@ -88,9 +92,11 @@ public class ProjectItemTermRepository_IntegTest extends CapexModuleIntegTestAbs
 
         // then sorted by date asc
         assertThat(firstItem.getProjectItemTerms()).hasSize(2);
+        assertThat(firstItem.getProjectItemTerms().get(0).getProjectItem()).isEqualTo(firstItem);
         assertThat(firstItem.getProjectItemTerms().get(0).getStartDate()).isEqualTo(startDate);
         assertThat(firstItem.getProjectItemTerms().get(0).getEndDate()).isEqualTo(endDate);
         assertThat(firstItem.getProjectItemTerms().get(0).getBudgetedAmount()).isEqualTo(amount);
+        assertThat(firstItem.getProjectItemTerms().get(1).getProjectItem()).isEqualTo(firstItem);
         assertThat(firstItem.getProjectItemTerms().get(1).getStartDate()).isEqualTo(startDate2);
         assertThat(firstItem.getProjectItemTerms().get(1).getEndDate()).isEqualTo(endDate2);
         assertThat(firstItem.getProjectItemTerms().get(1).getBudgetedAmount()).isEqualTo(amount2);
