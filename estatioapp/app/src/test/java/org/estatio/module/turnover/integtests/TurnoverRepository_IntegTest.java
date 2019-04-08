b373a6802e0f2d0c99512aa57b44a854b8611ee2
Johan Doornenbal <johan@filternet.nl>
2019-04-08 16:47:19 +0200

    ECP-962: first pass on persisting turnovers - sets up simple entity and repo

---
+++ estatioapp/app/src/test/java/org/estatio/module/turnover/integtests/TurnoverRepository_IntegTest.java
@@ -0,0 +1,101 @@
+/*
+ *
+ *  Copyright 2012-2014 Eurocommercial Properties NV
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.estatio.module.turnover.integtests;
+
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.services.registry.ServiceRegistry2;
+
+import org.estatio.module.currency.dom.Currency;
+import org.estatio.module.currency.fixtures.enums.Currency_enum;
+import org.estatio.module.lease.dom.Lease;
+import org.estatio.module.lease.fixtures.lease.enums.Lease_enum;
+import org.estatio.module.turnover.dom.Turnover;
+import org.estatio.module.turnover.dom.TurnoverRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TurnoverRepository_IntegTest extends TurnoverModuleIntegTestAbstract {
+
+    @Before
+    public void setupData() {
+        runFixtureScript(new FixtureScript() {
+            @Override
+            protected void execute(FixtureScript.ExecutionContext executionContext) {
+                executionContext.executeChild(this, Currency_enum.EUR.builder());
+                executionContext.executeChild(this, Currency_enum.SEK.builder());
+                executionContext.executeChild(this, Lease_enum.OxfTopModel001Gb.builder());
+            }
+        });
+    }
+
+    @Test
+    public void find_or_create_works() throws Exception {
+
+        // given
+        final Lease lease = Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2);
+        final LocalDate turnoverDate = new LocalDate(2019, 1, 1);
+        final Currency euro = Currency_enum.EUR.findUsing(serviceRegistry2);
+        final BigDecimal originalAmount = new BigDecimal("1234.56");
+        Turnover turnover = turnoverRepository.create(lease, turnoverDate, originalAmount, euro);
+
+        // when
+        final Currency sek = Currency_enum.SEK.findUsing(serviceRegistry2);
+        Turnover turnover2 = turnoverRepository.findOrCreate(lease, turnoverDate, new BigDecimal("4321.00"), sek);
+
+        // then
+        assertThat(turnover).isSameAs(turnover2);
+        assertThat(turnover2.getAmount()).isEqualTo(originalAmount);
+        assertThat(turnover2.getCurrency()).isEqualTo(euro);
+
+    }
+
+    @Test
+    public void upsert_works() throws Exception {
+
+        // given
+        final Lease lease = Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2);
+        final LocalDate turnoverDate = new LocalDate(2019, 1, 1);
+        final Currency euro = Currency_enum.EUR.findUsing(serviceRegistry2);
+        Turnover turnover = turnoverRepository.create(lease, turnoverDate, new BigDecimal("1234.56"), euro);
+
+        // when
+        final BigDecimal updatedAmount = new BigDecimal("4321.00");
+        final Currency sek = Currency_enum.SEK.findUsing(serviceRegistry2);
+        Turnover turnover2 = turnoverRepository.upsert(lease, turnoverDate, updatedAmount, sek);
+
+        // then
+        assertThat(turnover).isSameAs(turnover2);
+        assertThat(turnover2.getAmount()).isEqualTo(updatedAmount);
+        assertThat(turnover2.getCurrency()).isEqualTo(sek);
+
+    }
+
+    @Inject TurnoverRepository turnoverRepository;
+
+    @Inject ServiceRegistry2 serviceRegistry2;
+    
+}
