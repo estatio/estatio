fcc2f6b145b2ab932345d30eb03d320e2b216860
Johan Doornenbal <johan@filternet.nl>
2019-04-09 16:27:20 +0200

    ECP-962: adds turnover import view model and integration test

---
+++ estatioapp/app/src/main/java/org/estatio/module/turnover/imports/TurnoverImport.java
@@ -0,0 +1,196 @@
+package org.estatio.module.turnover.imports;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import com.google.common.collect.Lists;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Nature;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.user.UserService;
+
+import org.isisaddons.module.excel.dom.ExcelFixture;
+import org.isisaddons.module.excel.dom.ExcelFixture2;
+import org.isisaddons.module.excel.dom.ExcelFixtureRowHandler;
+import org.isisaddons.module.excel.dom.FixtureAwareRowHandler;
+
+import org.estatio.module.asset.dom.Unit;
+import org.estatio.module.asset.dom.UnitRepository;
+import org.estatio.module.base.dom.Importable;
+import org.estatio.module.currency.dom.Currency;
+import org.estatio.module.currency.dom.CurrencyRepository;
+import org.estatio.module.lease.dom.Lease;
+import org.estatio.module.lease.dom.LeaseRepository;
+import org.estatio.module.lease.dom.occupancy.Occupancy;
+import org.estatio.module.lease.dom.occupancy.OccupancyRepository;
+import org.estatio.module.turnover.dom.Turnover;
+import org.estatio.module.turnover.dom.TurnoverRepository;
+import org.estatio.module.turnover.dom.Type;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@DomainObject(
+        nature = Nature.VIEW_MODEL,
+        objectType = "org.estatio.module.turnover.imports.TurnoverImport"
+)
+public class TurnoverImport implements Importable, ExcelFixtureRowHandler, FixtureAwareRowHandler<TurnoverImport> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TurnoverImport.class);
+
+    @Getter @Setter
+    private String leaseReference;
+
+    @Getter @Setter
+    private String unitReference;
+
+    @Getter @Setter
+    private LocalDate date;
+
+    @Getter @Setter
+    private BigDecimal turnoverGrossAmount;
+
+    @Getter @Setter
+    private BigDecimal turnoverNetAmount;
+
+    @Getter @Setter
+    private String type;
+
+    @Getter @Setter
+    private String currencyReference;
+
+    @Getter @Setter
+    private int nonComparableFlag;
+
+    @Getter @Setter
+    private BigInteger turnoverPurchaseCount;
+
+    @Getter @Setter
+    private String comments;
+
+    @Programmatic
+    @Override
+    public List<Object> importData(final Object previousRow) {
+
+        Type typeEnum;
+
+        try {
+            typeEnum = Type.valueOf(type);
+        } catch (Exception e){
+            logAndWarn(String.format("Type not found for %s", type));
+            return Lists.newArrayList();
+        }
+
+        final Lease lease = leaseRepository.findLeaseByReference(leaseReference);
+        if (lease==null) {
+            logAndWarn(String.format("Lease not found for reference %s", leaseReference));
+            return Lists.newArrayList();
+        }
+
+        final Unit unit = unitRepository.findUnitByReference(unitReference);
+        if (unit==null) {
+            logAndWarn(String.format("Unit not found for reference %s", unitReference));
+            return Lists.newArrayList();
+        }
+
+        final Currency currency = currencyRepository.findCurrency(currencyReference);
+        if (currency==null) {
+            logAndWarn(String.format("Currency not found for reference %s", currencyReference));
+            return Lists.newArrayList();
+        }
+
+        final List<Occupancy> activeOccupanciesForLeaseOnDateAndUnit = occupancyRepository.findByLeaseAndDate(lease, date)
+                .stream()
+                .filter(o->o.getUnit().equals(unit))
+                .filter(o->o.getEndDate()==null || !o.getEndDate().isBefore(date))
+                .collect(Collectors.toList());
+
+        if (activeOccupanciesForLeaseOnDateAndUnit.isEmpty()){
+            logAndWarn(String.format("No active occupancy found for lease %s and unit %s on date %s", leaseReference, unitReference, date));
+            return Lists.newArrayList();
+        }
+
+        if (activeOccupanciesForLeaseOnDateAndUnit.size()>1){
+            logAndWarn(String.format("Multiple active occupancies found for lease %s and unit %s on date %s", leaseReference, unitReference, date));
+            return Lists.newArrayList();
+        }
+
+        Occupancy occupancy = activeOccupanciesForLeaseOnDateAndUnit.get(0);
+        Turnover turnover = turnoverRepository.create(
+                occupancy,
+                date,
+                typeEnum,
+                LocalDateTime.now(),
+                userService.getUser().getName(),
+                currency,
+                turnoverNetAmount.equals(BigDecimal.ZERO) ? null : turnoverNetAmount,
+                turnoverGrossAmount.equals(BigDecimal.ZERO) ? null : turnoverGrossAmount,
+                turnoverPurchaseCount.equals(BigInteger.ZERO) ? null : turnoverPurchaseCount,
+                comments,
+                nonComparableFlag > 0 ? true: false);
+
+        return Lists.newArrayList(turnover);
+    }
+
+    private void logAndWarn(final String message) {
+        LOG.warn(message);
+        messageService.warnUser(message);
+    }
+
+    @Override
+    public List<Object> handleRow(final FixtureScript.ExecutionContext executionContext, final ExcelFixture excelFixture, final Object previousRow) {
+        return importData(previousRow);
+    }
+
+    /**
+     * To allow for usage within fixture scripts also.
+     */
+    @Setter
+    private FixtureScript.ExecutionContext executionContext;
+
+    /**
+     * To allow for usage within fixture scripts also.
+     */
+    @Setter
+    private ExcelFixture2 excelFixture2;
+
+    @Override
+    public void handleRow(final TurnoverImport previousRow) {
+
+        if (executionContext != null && excelFixture2 != null) {
+            if (executionContext.getParameterAsBoolean("testMode") != null && executionContext.getParameterAsBoolean("testMode")) {
+                executionContext.addResult(excelFixture2, this.importData(previousRow));
+            } else {
+                this.importData(previousRow);
+            }
+        }
+
+    }
+
+    @Inject OccupancyRepository occupancyRepository;
+
+    @Inject LeaseRepository leaseRepository;
+
+    @Inject UnitRepository unitRepository;
+
+    @Inject CurrencyRepository currencyRepository;
+
+    @Inject MessageService messageService;
+
+    @Inject TurnoverRepository turnoverRepository;
+
+    @Inject UserService userService;
+
+}
---
+++ estatioapp/app/src/test/java/org/estatio/module/turnover/integtests/TurnoverImport_IntegTest.java
@@ -0,0 +1,95 @@
+/*
+ *
+ *  Copyright 2012-2014 Eurocommercial Properties NV
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.estatio.module.turnover.integtests;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.services.registry.ServiceRegistry2;
+
+import org.estatio.module.currency.fixtures.enums.Currency_enum;
+import org.estatio.module.lease.fixtures.lease.enums.Lease_enum;
+import org.estatio.module.turnover.dom.Turnover;
+import org.estatio.module.turnover.dom.TurnoverRepository;
+import org.estatio.module.turnover.dom.Type;
+import org.estatio.module.turnover.fixtures.TurnoverImportXlsxFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TurnoverImport_IntegTest extends TurnoverModuleIntegTestAbstract {
+
+    @Before
+    public void setupData() {
+        runFixtureScript(new FixtureScript() {
+            @Override
+            protected void execute(ExecutionContext executionContext) {
+                executionContext.executeChild(this, Currency_enum.EUR.builder());
+                executionContext.executeChild(this, Lease_enum.OxfTopModel001Gb.builder());
+                executionContext.executeChild(this, new TurnoverImportXlsxFixture());
+            }
+        });
+    }
+
+    @Test
+    public void import_succeeded() throws Exception {
+
+        final List<Turnover> turnovers = turnoverRepository.listAll();
+        assertThat(turnovers).hasSize(2);
+        final Turnover first = turnovers.get(0);
+        assertThat(first.getOccupancy().getLease()).isEqualTo(Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2));
+        assertThat(first.getOccupancy().getUnit().getReference()).isEqualTo("OXF-001");
+        assertThat(first.getDate()).isEqualTo(new LocalDate(2019,1,1));
+        assertThat(first.getType()).isEqualTo(Type.PRELIMINARY);
+        assertThat(first.getReportedAt().toLocalDate()).isEqualTo(LocalDate.now());
+        assertThat(first.getReportedBy()).isEqualTo("tester");
+        assertThat(first.getCurrency()).isEqualTo(Currency_enum.EUR.findUsing(serviceRegistry2));
+        assertThat(first.getTurnoverNetAmount()).isEqualTo(new BigDecimal("12345.56"));
+        assertThat(first.getTurnoverGrossAmount()).isEqualTo(new BigDecimal("14814.67"));
+        assertThat(first.getTurnoverPurchaseCount()).isEqualTo(BigInteger.valueOf(123));
+        assertThat(first.getComments()).isNull();
+        assertThat(first.isNonComparable()).isFalse();
+
+        final Turnover second = turnovers.get(1);
+        assertThat(second.getOccupancy().getLease()).isEqualTo(Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2));
+        assertThat(second.getOccupancy().getUnit().getReference()).isEqualTo("OXF-001");
+        assertThat(second.getDate()).isEqualTo(new LocalDate(2019,4,1));
+        assertThat(second.getType()).isEqualTo(Type.AUDITED);
+        assertThat(second.getReportedAt().toLocalDate()).isEqualTo(LocalDate.now());
+        assertThat(second.getReportedBy()).isEqualTo("tester");
+        assertThat(second.getCurrency()).isEqualTo(Currency_enum.GBP.findUsing(serviceRegistry2));
+        assertThat(second.getTurnoverNetAmount()).isNull();
+        assertThat(second.getTurnoverGrossAmount()).isEqualTo(new BigDecimal("2345.67"));
+        assertThat(second.getTurnoverPurchaseCount()).isNull();
+        assertThat(second.getComments()).isEqualTo("This is non comparable because of some reason");
+        assertThat(second.isNonComparable()).isTrue();
+    }
+
+    @Inject TurnoverRepository turnoverRepository;
+
+    @Inject ServiceRegistry2 serviceRegistry2;
+    
+}
