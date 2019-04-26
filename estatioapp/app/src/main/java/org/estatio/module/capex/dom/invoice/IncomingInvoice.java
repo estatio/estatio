c88b0c147e393b02ed784c1488fcef6620bc892b
danhaywood <dan@haywood-associates.co.uk>
2019-04-26 14:28:39 +0100

    Revert "ECP-981: enables Italian users to split and merge incoming invoice items even when paid"
    This reverts commit 3e8ed6ac3c7694493a1a11d5b9970f5332b16a5d.

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoice.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoice.java
@@ -1129,7 +1129,7 @@ public class IncomingInvoice extends Invoice<IncomingInvoice> implements SellerB
     public String disableSplitItem() {
 
         ReasonBuffer2 buf = ReasonBuffer2.forSingle("Cannot split items because");
-        if (!CountryUtil.isItalian(this)) reasonDisabledDueToApprovalStateIfAny(this, buf); // we slack this constraint so Italian users can modify when already paid for reporting reasons
+        reasonDisabledDueToApprovalStateIfAny(this, buf);
         buf.append(() -> choices0SplitItem().isEmpty(), "there are no items");
         return buf.getReason();
     }
@@ -1235,7 +1235,7 @@ public class IncomingInvoice extends Invoice<IncomingInvoice> implements SellerB
         final ReasonBuffer2 buf = ReasonBuffer2.forSingle("Cannot merge items because");
 
         final Object viewContext = this;
-        if (!CountryUtil.isItalian(this)) reasonDisabledDueToApprovalStateIfAny(viewContext, buf); // we slack this constraint so Italian users can modify when already paid for reporting reasons
+        reasonDisabledDueToApprovalStateIfAny(viewContext, buf);
 
         buf.append(() -> getItems().size() < 2, "merging needs 2 or more items");
         return buf.getReason();
