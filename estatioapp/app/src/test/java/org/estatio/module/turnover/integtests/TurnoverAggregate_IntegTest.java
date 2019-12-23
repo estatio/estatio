bdd293a90a9cb788965750124e26c470fc1ac259
Johan Doornenbal <johan@yodo.nl>
2019-12-23 13:09:42 +0100

    ECP-1125: setup 2 additional parent leases

--- estatioapp/app/src/test/java/org/estatio/module/turnover/integtests/TurnoverAggregate_IntegTest.java
+++ estatioapp/app/src/test/java/org/estatio/module/turnover/integtests/TurnoverAggregate_IntegTest.java
@@ -77,20 +77,35 @@ public class TurnoverAggregate_IntegTest extends TurnoverModuleIntegTestAbstract
         // occ2 starts the day after on 2017,4,17
         // turnovers 2019,5,1 and later are on occ2
         // occ3 runs parallel to occ2 and has turnovers on it as well starting 2019,4,1
+        // lease 2 and occ4 start on 2019,11,10
+        // lease 3 and occ5 start on 2020,5,20
         final LocalDate endDateOcc1 = new LocalDate(2017, 4, 16);
-        final Lease oxfTopModel = Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2);
-        final Occupancy occ1 = oxfTopModel.getOccupancies().first();
+        final Lease oxfTopModelLease1 = Lease_enum.OxfTopModel001Gb.findUsing(serviceRegistry2);
+        final Occupancy occ1 = oxfTopModelLease1.getOccupancies().first();
         occ1.terminate(endDateOcc1);
         final Property oxf = Property_enum.OxfGb.findUsing(serviceRegistry2);
 
         final Unit new_unit_for_topmodel = oxf.newUnit("OXF-001A", "New unit 1 for Topmodel", UnitType.BOUTIQUE);
-        final Occupancy occ2 = oxfTopModel.newOccupancy(endDateOcc1.plusDays(1), new_unit_for_topmodel);
+        final Occupancy occ2 = oxfTopModelLease1.newOccupancy(endDateOcc1.plusDays(1), new_unit_for_topmodel);
         mixin(Occupancy_createTurnoverReportingConfig.class,occ2).createTurnoverReportingConfig(Type.PRELIMINARY, occ2.getStartDate(), Frequency.MONTHLY, Currency_enum.EUR.findUsing(serviceRegistry2));
 
         final Unit par_unit_for_topmodel = oxf.newUnit("OXF-001A-PAR", "Parallel unit 1 for Topmodel", UnitType.BOUTIQUE);
-        final Occupancy occ3 = oxfTopModel.newOccupancy(endDateOcc1.plusDays(1), par_unit_for_topmodel);
+        final Occupancy occ3 = oxfTopModelLease1.newOccupancy(endDateOcc1.plusDays(1), par_unit_for_topmodel);
         mixin(Occupancy_createTurnoverReportingConfig.class,occ3).createTurnoverReportingConfig(Type.PRELIMINARY, occ3.getStartDate(), Frequency.MONTHLY, Currency_enum.EUR.findUsing(serviceRegistry2));
 
+        final LocalDate startDateLease2 = new LocalDate(2019, 11, 10);
+        final LocalDate startDateLease3 = new LocalDate(2020, 5, 20);
+
+        final Lease oxfTopModelLease2 = wrap(oxfTopModelLease1).renew("OXF-TOPMODEL-2", "Lease 2",
+                startDateLease2, startDateLease3.minusDays(1));
+        final Occupancy occ4 = oxfTopModelLease2.getOccupancies().first();
+        mixin(Occupancy_createTurnoverReportingConfig.class,occ4).createTurnoverReportingConfig(Type.PRELIMINARY, occ4.getStartDate(), Frequency.MONTHLY, Currency_enum.EUR.findUsing(serviceRegistry2));
+
+        final Lease oxfTopModelLease3 = wrap(oxfTopModelLease2).renew("OXF-TOPMODEL-3", "Lease 3",
+                startDateLease3, startDateLease3.plusYears(1));
+        final Occupancy occ5 = oxfTopModelLease3.getOccupancies().first();
+        mixin(Occupancy_createTurnoverReportingConfig.class,occ5).createTurnoverReportingConfig(Type.PRELIMINARY, occ5.getStartDate(), Frequency.MONTHLY, Currency_enum.EUR.findUsing(serviceRegistry2));
+        
         runFixtureScript(new FixtureScript() {
             @Override
             protected void execute(ExecutionContext executionContext) {
@@ -99,10 +114,12 @@ public class TurnoverAggregate_IntegTest extends TurnoverModuleIntegTestAbstract
         });
 
         final List<Turnover> turnovers = turnoverRepository.listAll();
-        assertThat(turnovers).hasSize(67);
+        assertThat(turnovers).hasSize(76);
         final List<Turnover> turnoversOnOcc1 = mixin(Occupancy_turnovers.class, occ1).turnovers();
         final List<Turnover> turnoversOnOcc2 = mixin(Occupancy_turnovers.class, occ2).turnovers();
         final List<Turnover> turnoversOnOcc3 = mixin(Occupancy_turnovers.class, occ3).turnovers();
+        final List<Turnover> turnoversOnOcc4 = mixin(Occupancy_turnovers.class, occ4).turnovers();
+        final List<Turnover> turnoversOnOcc5 = mixin(Occupancy_turnovers.class, occ5).turnovers();
         assertThat(turnoversOnOcc1).hasSize(52);
         final LocalDate maxDateOcc1 = turnoversOnOcc1.stream().max(Comparator.comparing(Turnover::getDate))
                 .map(Turnover::getDate).get();
@@ -117,6 +134,16 @@ public class TurnoverAggregate_IntegTest extends TurnoverModuleIntegTestAbstract
         final LocalDate minDateOcc3 = turnoversOnOcc3.stream().min(Comparator.comparing(Turnover::getDate))
                 .map(Turnover::getDate).get();
         assertThat(minDateOcc3).isEqualTo(new LocalDate(2019, 4,1));
+
+        assertThat(turnoversOnOcc4).hasSize(7);
+        final LocalDate minDateOcc4 = turnoversOnOcc4.stream().min(Comparator.comparing(Turnover::getDate))
+                .map(Turnover::getDate).get();
+        assertThat(minDateOcc4).isEqualTo(new LocalDate(2019, 11,1));
+
+        assertThat(turnoversOnOcc5).hasSize(2);
+        final LocalDate minDateOcc5 = turnoversOnOcc5.stream().min(Comparator.comparing(Turnover::getDate))
+                .map(Turnover::getDate).get();
+        assertThat(minDateOcc5).isEqualTo(new LocalDate(2020, 5,1));
 
         // when (aggregate)
 
