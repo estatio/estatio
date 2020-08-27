51d704618bf1e3a633efbbe2819390bf32ad36b3
Johan Doornenbal <johan@yodo.nl>
2020-08-27 15:20:56 +0200

    ECP-1234: adds first sketch

---
+++ estatioapp/app/src/main/java/org/estatio/module/lease/dom/amortisation/AmortisationEntry.java
@@ -0,0 +1,66 @@
+package org.estatio.module.lease.dom.amortisation;
+
+import java.math.BigDecimal;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.DatastoreIdentity;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Queries;
+import javax.jdo.annotations.Query;
+import javax.jdo.annotations.Unique;
+import javax.jdo.annotations.Version;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.joda.time.LocalDate;
+
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@PersistenceCapable(
+        identityType = IdentityType.DATASTORE,
+        schema = "dbo",
+        table = "AmortisationEntry"
+)
+@DatastoreIdentity(
+        strategy = IdGeneratorStrategy.IDENTITY,
+        column = "id")
+@Version(
+        strategy = VersionStrategy.VERSION_NUMBER,
+        column = "version")
+@Queries({
+        @Query(
+                name = "findBySchedule", language = "JDOQL",
+                value = "SELECT "
+                        + "FROM org.estatio.module.lease.dom.amortisation.AmortisationEntry "
+                        + "WHERE schedule == :schedule "),
+})
+@Unique(name = "AmortisationEntry_schedule_entryDate_UNQ", members = { "schedule", "entryDate" })
+@DomainObject(
+        editing = Editing.DISABLED,
+        objectType = "amortisation.AmortisationEntry"
+)
+@DomainObjectLayout(
+        bookmarking = BookmarkPolicy.AS_CHILD
+)
+public class AmortisationEntry {
+
+    @Getter @Setter
+    @Column(name = "scheduleId", allowsNull = "false")
+    private AmortisationSchedule schedule;
+
+    @Getter @Setter
+    @Column(allowsNull = "false")
+    private LocalDate entryDate;
+
+    @Getter @Setter
+    @Column(allowsNull = "false", scale = 2)
+    private BigDecimal entryAmount;
+
+}
---
+++ estatioapp/app/src/main/java/org/estatio/module/lease/dom/amortisation/AmortisationSchedule.java
@@ -0,0 +1,99 @@
+package org.estatio.module.lease.dom.amortisation;
+
+import java.math.BigDecimal;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.DatastoreIdentity;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.Queries;
+import javax.jdo.annotations.Query;
+import javax.jdo.annotations.Unique;
+import javax.jdo.annotations.Version;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.joda.time.LocalDate;
+
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+
+import org.isisaddons.module.security.dom.tenancy.ApplicationTenancy;
+
+import org.estatio.module.base.dom.UdoDomainObject2;
+import org.estatio.module.lease.dom.Frequency;
+import org.estatio.module.lease.dom.LeaseItem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@PersistenceCapable(
+        identityType = IdentityType.DATASTORE,
+        schema = "dbo",
+        table = "AmortisationSchedule"
+)
+@DatastoreIdentity(
+        strategy = IdGeneratorStrategy.IDENTITY,
+        column = "id")
+@Version(
+        strategy = VersionStrategy.VERSION_NUMBER,
+        column = "version")
+@Queries({
+        @Query(
+                name = "findByLeaseItem", language = "JDOQL",
+                value = "SELECT "
+                        + "FROM org.estatio.module.lease.dom.amortisation.AmortisationSchedule "
+                        + "WHERE leaseItem == :leaseItem "),
+})
+@Unique(name = "AmortisationSchedule_leaseItem_UNQ", members = { "leaseItem" })
+@DomainObject(
+        editing = Editing.DISABLED,
+        objectType = "amortisation.AmortisationSchedule"
+)
+@DomainObjectLayout(
+        bookmarking = BookmarkPolicy.AS_ROOT
+)
+public class AmortisationSchedule extends UdoDomainObject2<AmortisationSchedule> {
+
+    public AmortisationSchedule() {
+        super("leaseItem");
+    }
+
+//    TODO: this will only be possible when we refer the creation of a schedule to when an amendment is applied and the
+//    (Discount) leaseItem created
+//    Another approach would be to store the amendment item when the amendment state moves to Signed and store leaseItem when moving to applied
+//    CON: this could lead to changes in the scheduled amount and correction of transactions ....
+    @Getter @Setter
+    @Column(name = "leaseItemId", allowsNull = "false")
+    private LeaseItem leaseItem;
+
+    @Getter @Setter
+    @Column(allowsNull = "false", scale = 2)
+    private BigDecimal scheduledAmount;
+
+    @Getter @Setter
+    @Column(allowsNull = "false")
+    private Frequency frequency;
+
+    @Getter @Setter
+    @Column(allowsNull = "false")
+    private LocalDate startDate;
+
+    @Getter @Setter
+    @Column(allowsNull = "false")
+    private LocalDate endDate;
+
+    @Persistent(mappedBy = "schedule", dependentElement = "true")
+    @Getter @Setter
+    private SortedSet<AmortisationEntry> entries = new TreeSet<>();
+
+    @Override
+    public ApplicationTenancy getApplicationTenancy() {
+        return leaseItem.getApplicationTenancy();
+    }
+}
