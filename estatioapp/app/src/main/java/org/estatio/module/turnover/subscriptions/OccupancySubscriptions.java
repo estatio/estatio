e4b8cdf45a5a8c127716aa1c8b4ac4d7efc516c9
Johan Doornenbal <johan@yodo.nl>
2020-09-21 17:57:56 +0200

    ECP-1250: now removes 'empty' turnover configs when removing an occuppancy

--- estatioapp/app/src/main/java/org/estatio/module/turnover/subscriptions/OccupancySubscriptions.java
+++ estatioapp/app/src/main/java/org/estatio/module/turnover/subscriptions/OccupancySubscriptions.java
@@ -6,6 +6,8 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.services.factory.FactoryService;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
 
 import org.estatio.module.currency.dom.CurrencyRepository;
 import org.estatio.module.currency.fixtures.enums.Currency_enum;
@@ -13,6 +15,7 @@ import org.estatio.module.lease.dom.occupancy.Occupancy;
 import org.estatio.module.lease.dom.occupancy.OccupancyRepository;
 import org.estatio.module.turnover.contributions.Occupancy_createTurnoverReportingConfig;
 import org.estatio.module.turnover.dom.Frequency;
+import org.estatio.module.turnover.dom.TurnoverReportingConfig;
 import org.estatio.module.turnover.dom.TurnoverReportingConfigRepository;
 import org.estatio.module.turnover.dom.Type;
 
@@ -65,9 +68,38 @@ public class OccupancySubscriptions extends org.apache.isis.applib.AbstractSubsc
                 );
     }
 
+    @Programmatic
+    @com.google.common.eventbus.Subscribe
+    @org.axonframework.eventhandling.annotation.EventHandler
+    public void on(final Occupancy.RemoveEvent ev) {
+        final Occupancy occupancyToBeDeleted = ev.getSource();
+        switch (ev.getEventPhase()) {
+        case VALIDATE:
+            final TurnoverReportingConfig configWithTurnoversReported = turnoverReportingConfigRepository
+                    .findByOccupancy(occupancyToBeDeleted)
+                    .stream()
+                    .filter(rc -> !rc.getTurnovers().isEmpty())
+                    .findFirst().orElse(null);
+            if (configWithTurnoversReported!=null) messageService.warnUser("Cannot remove occupancy: there are turnovers reported.");
+            break;
+        case EXECUTING:
+            turnoverReportingConfigRepository.findByOccupancy(occupancyToBeDeleted)
+            .stream()
+            .filter(rc->rc.getTurnovers().isEmpty())
+            .forEach(rc->repositoryService.removeAndFlush(rc));
+            break;
+        default:
+            break;
+        }
+    }
+
     @Inject TurnoverReportingConfigRepository turnoverReportingConfigRepository;
 
     @Inject FactoryService factoryService;
 
     @Inject CurrencyRepository currencyRepository;
+
+    @Inject RepositoryService repositoryService;
+
+    @Inject MessageService messageService;
 }
