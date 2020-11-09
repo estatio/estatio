b9adac12bfe833f3acb12382469c4e3843049b46
Johan Doornenbal <johan@yodo.nl>
2020-11-09 21:52:49 +0100

    ECP-1260: adds objecttype

--- estatioapp/app/src/main/java/org/estatio/module/application/app/dashboard/TenantReferenceMappingLine.java
+++ estatioapp/app/src/main/java/org/estatio/module/application/app/dashboard/TenantReferenceMappingLine.java
@@ -5,7 +5,8 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.isis.applib.annotation.ViewModel;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Nature;
 
 import org.estatio.module.party.dom.Party;
 import org.estatio.module.party.dom.PartyRepository;
@@ -13,7 +14,7 @@ import org.estatio.module.party.dom.PartyRepository;
 import lombok.Getter;
 import lombok.Setter;
 
-@ViewModel
+@DomainObject(nature = Nature.VIEW_MODEL, objectType = "dashboard.TenantReferenceMappingLine")
 public class TenantReferenceMappingLine {
 
     public static Logger LOG = LoggerFactory.getLogger(TenantReferenceMappingLine.class);
