96bbb508f097648cfd31ecb61e7be8f96625593f
Johan Doornenbal <johan@yodo.nl>
2021-02-23 10:57:01 +0100

    ECP-1346: removes legacy code that was accidentally copied but not deleted from ApplicationTenancyEvaluatorForEstatio

--- estatioapp/app/src/main/java/org/estatio/module/base/spiimpl/security/ApplicationTenancyEvaluatorForEstatio.java
+++ estatioapp/app/src/main/java/org/estatio/module/base/spiimpl/security/ApplicationTenancyEvaluatorForEstatio.java
@@ -16,7 +16,6 @@
  */
 package org.estatio.module.base.spiimpl.security;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Callable;
@@ -34,72 +33,17 @@ import org.isisaddons.module.security.dom.tenancy.ApplicationTenancyEvaluator;
 import org.isisaddons.module.security.dom.tenancy.HasAtPath;
 import org.isisaddons.module.security.dom.user.ApplicationUser;
 
-import org.estatio.module.asset.dom.Property;
-import org.estatio.module.asset.dom.role.FixedAssetRoleRepository;
-import org.estatio.module.capex.app.ExternalUserService;
-import org.estatio.module.capex.dom.invoice.IncomingInvoice;
-import org.estatio.module.capex.dom.invoice.IncomingInvoiceItem;
-import org.estatio.module.capex.dom.invoice.approval.IncomingInvoiceApprovalConfigurationUtil;
-import org.estatio.module.capex.dom.order.Order;
-import org.estatio.module.capex.dom.order.OrderItem;
-import org.estatio.module.invoice.dom.InvoiceItem;
-import org.estatio.module.party.dom.PersonRepository;
-import org.estatio.module.task.dom.state.StateTransition;
-import org.estatio.module.task.dom.state.StateTransitionService;
-import org.estatio.module.task.dom.task.Task;
-
 @DomainService(nature = NatureOfService.DOMAIN, menuOrder = "99", objectType = "security.ApplicationTenancyEvaluatorForEstatio")
 public class ApplicationTenancyEvaluatorForEstatio implements ApplicationTenancyEvaluator {
 
     @Inject
     QueryResultsCache queryResultsCache;
 
-    @Inject
-    PersonRepository personRepository;
-
-    @Inject
-    FixedAssetRoleRepository fixedAssetRoleRepository;
-
-    @Inject
-    StateTransitionService stateTransitionService;
-
-    @Inject ExternalUserService externalUserService;
-
     public boolean handles(Class<?> cls) {
         return HasAtPath.class.isAssignableFrom(cls);
     }
 
     public String hides(Object domainObject, ApplicationUser applicationUser) {
-
-        if (applicationUser.getUsername().contains("external.ecpnv") && applicationUser.getAtPath().startsWith("/ITA")){
-
-            final List<Property> properties = propertiesForUserAsPersonCached(applicationUser);
-
-            if (properties.isEmpty()) return "No property could be derived for user";
-
-
-            if (domainObject instanceof IncomingInvoice){
-
-                IncomingInvoice invoice = (IncomingInvoice) domainObject;
-                if (!invoiceVisibleForExternalUser(invoice, properties)) return "Invoice not visible for user";
-
-            }
-
-            if (domainObject instanceof Order){
-
-                Order order = (Order) domainObject;
-                if (!orderVisibleForExternalUser(order, properties)) return "Order not visible for user";
-
-            }
-
-            if (domainObject instanceof Task){
-
-                Task task = (Task) domainObject;
-                if (!taskVisibleForExternalUser(task, properties)) return "Task not visible for user";
-
-            }
-
-        }
 
         final String objectTenancyPath = applicationTenancyPathForCached(domainObject);
         if(objectTenancyPath == null) {
@@ -114,74 +58,6 @@ public class ApplicationTenancyEvaluatorForEstatio implements ApplicationTenancy
             return null;
         }
         return String.format("User with tenancy \'%s\' is not permitted to view object with tenancy \'%s\'", userTenancyPath, objectTenancyPath);
-    }
-
-    boolean taskVisibleForExternalUser(final Task task, final List<Property> propertiesForUser){
-
-        final StateTransition stateTransition = stateTransitionService.findFor(task);
-        if (stateTransition==null) return false;
-
-        final Object domainObject = stateTransition.getDomainObject();
-
-        if (domainObject instanceof IncomingInvoice){
-            IncomingInvoice invoice = (IncomingInvoice) domainObject;
-            return invoiceVisibleForExternalUser(invoice, propertiesForUser);
-        }
-
-        if (domainObject instanceof Order){
-            Order order = (Order) domainObject;
-            return orderVisibleForExternalUser(order, propertiesForUser);
-        }
-
-        return false;
-    }
-
-    boolean invoiceVisibleForExternalUser(final IncomingInvoice invoice, final List<Property> propertiesForUser){
-
-        final Property propertyOnInvoice = invoice.getProperty();
-        if (propertyOnInvoice == null ) return false;
-
-        if (!IncomingInvoiceApprovalConfigurationUtil.isInvoiceForExternalCenterManager(invoice)) return false;
-
-        if (propertiesForUser.contains(propertyOnInvoice)) return true;
-
-        return false;
-    }
-
-    boolean orderVisibleForExternalUser(final Order order, final List<Property> propertiesForUser){
-
-        final Property propertyOnOrder = order.getProperty();
-        if (propertyOnOrder == null ) return false;
-
-        final List<String> propertyRefsFound = new ArrayList<>();
-        IncomingInvoiceApprovalConfigurationUtil.PROPERTY_REF_EXTERNAL_PROJECT_REF_MAP.forEach((k,v)->{
-            if (k.equals(propertyOnOrder.getReference()) && atLeastOneItemHasProjectWithReference(order, v)) propertyRefsFound.add(k);
-        });
-
-        if (propertyRefsFound.isEmpty()) return false;
-
-        if (propertiesForUser.contains(propertyOnOrder)) return true;
-
-        return false;
-    }
-
-    private boolean atLeastOneItemHasProjectWithReference(final IncomingInvoice invoice, final String projectRefExt) {
-        for (InvoiceItem ii : invoice.getItems()){
-            IncomingInvoiceItem cii = (IncomingInvoiceItem) ii;
-            if (cii.getProject()!=null && cii.getProject().getReference().equals(projectRefExt)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean atLeastOneItemHasProjectWithReference(final Order order, final String projectRefExt) {
-        for (OrderItem oi : order.getItems()){
-            if (oi.getProject()!=null && oi.getProject().getReference().equals(projectRefExt)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public String disables(Object domainObject, ApplicationUser applicationUser) {
@@ -281,18 +157,7 @@ public class ApplicationTenancyEvaluatorForEstatio implements ApplicationTenancy
                             .toList();
     }
 
-    //region > helpers: propertiesForUserAsPersonCached, propertiesForUserAsPerson, applicationTenancyPathForCached, applicationTenancyPathFor, userTenancyPathForCached, userTenancyPathFor
-
-    List<Property> propertiesForUserAsPersonCached(final ApplicationUser applicationUser){
-        return (List<Property>)queryResultsCache.execute(
-                (Callable) () -> propertiesForUserAsPerson(applicationUser),
-                ApplicationTenancyEvaluatorForEstatio.class,
-                "propertiesForUserAsPersonCached", applicationUser);
-    }
-
-    private List<Property> propertiesForUserAsPerson(final ApplicationUser applicationUser){
-        return externalUserService.getPropertiesForExternalUser();
-    }
+    //region > helpers: applicationTenancyPathForCached, applicationTenancyPathFor, userTenancyPathForCached, userTenancyPathFor
 
     private String applicationTenancyPathForCached(final Object domainObject) {
         return (String)queryResultsCache.execute(
